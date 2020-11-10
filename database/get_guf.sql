8e647eeb3e85fee590c8a756a0887fedab21c307
matthias_schaub <matthias.schaub@posteo.de>
2020-11-10 13:26:35 +0100

    wip

--- database/get_guf.sql
+++
@@ -1,8 +0,0 @@
-COPY (
-    SELECT
-        *
-    FROM
-        public.guf04
-    WHERE
-        rast && ST_Transform (ST_MakeEnvelope (5.954590, 47.294134, 15.227051, 55.002826, 4326), 954009))
-TO 'guf-heidelberg.copy';
