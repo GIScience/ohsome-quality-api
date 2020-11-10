132bef890171f88352a31ef462fb45ec0f06aa2f
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2020-11-10 13:16:49 +0100

    Add Tanzania boudary geometry.

---
+++ database/get_bbox.sh
@@ -0,0 +1,10 @@
+##
+echo "Create bounding box of Tanzania"
+##
+# Extract extent (bbox) from ogrinfo
+# Bbox format: lon1 lat1 lon2 lat2
+bbox_comma="$(ogrinfo -al -so TZA_adm0.geojson | grep Extent | awk -v ORS="," -v RS=")" -v FS="(" '{print $2}' | sed -r 's/,/ /g' | xargs | sed -r 's/ /,/g')"
+bbox="$(echo $bbox_comma | sed 's/,/ /g')"
+echo "Bounding Box: $bbox"
+echo $bbox > bbox.txt
+echo -e "Done!\n"
