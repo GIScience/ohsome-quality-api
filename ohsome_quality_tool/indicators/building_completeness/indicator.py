19735d87b9897fb2b71d57d767dd642cf85f0446
klerings <klerings@cl.uni-heidelberg.de>
2020-11-13 17:40:52 +0100

    ohsome query for building completeness

--- ohsome_quality_tool/indicators/building_completeness/indicator.py
+++ ohsome_quality_tool/indicators/building_completeness/indicator.py
@@ -1,4 +1,5 @@
 from geojson import FeatureCollection
+from ohsome import OhsomeClient
 
 from ohsome_quality_tool.base.indicator import BaseIndicator
 from ohsome_quality_tool.utils.definitions import logger
@@ -15,6 +16,7 @@ class Indicator(BaseIndicator):
         bpolys: FeatureCollection = None,
         table: str = None,
         feature_id: int = None,
+        osm_building_area: float = 0.0,
     ) -> None:
         super().__init__(
             dynamic=dynamic, bpolys=bpolys, table=table, feature_id=feature_id
@@ -22,6 +24,13 @@ class Indicator(BaseIndicator):
 
     def preprocess(self):
         logger.info(f"run preprocessing for {self.name} indicator")
+
+        client = OhsomeClient()
+        response = client.elements.area.post(bpolys=self.bpolys, filter="buildings=*")
+        self.osm_building_area = response.as_dataframe().iloc[0]["value"]
+        logger.info(f"extracted osm features for {self.name} indicator")
+
+        # TODO: obtain Global Urban Footprint data
 
     def calculate(self):
         logger.info(f"run calculation for {self.name} indicator")
