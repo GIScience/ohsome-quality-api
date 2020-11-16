1f1fee0702dab19a15a80c4112e515a36d244470
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2020-11-16 14:06:16 +0100

    Add import script for admin boundaries.

---
+++ scripts/import/NUTS_2021.sh
@@ -0,0 +1,8 @@
+wget https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_01M_2021_4326.geojson
+wget https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_4326.geojson
+
+ogr2ogr -f PostgreSQL PG:"host=localhost port=5432 dbname=hexadmin user=hexadmin" NUTS_RG_01M_2021_4326.geojson
+ogr2ogr -f PostgreSQL PG:"host=localhost port=5432 dbname=hexadmin user=hexadmin" NUTS_RG_60M_2021_4326.geojson
+
+rm NUTS_RG_01M_2021_4326.geojson
+rm NUTS_RG_60M_2021_4326.geojson
