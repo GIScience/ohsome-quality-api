0ae2beb4bcc24770587bae7e93fca49482c236d6
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2020-11-16 14:06:52 +0100

    Cleaning files.

--- scripts/import/NUTS-2021.sh
+++
@@ -1,5 +0,0 @@
-wget https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_01M_2021_4326.geojson
-wget https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_4326.geojson
-
-ogr2ogr -f PostgreSQL PG:"dbname=ohsome-hex user=hexadmin" NUTS_RG_01M_2021_4326.geojson
-ogr2ogr -f PostgreSQL PG:"dbname=ohsome-hex user=hexadmin" NUTS_RG_60M_2021_4326.geojson
