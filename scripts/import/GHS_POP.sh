63248cb4839c55da323802cff68ee5e5999eab13
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2020-11-17 12:02:15 +0100

    Change srid from 954009 to 54009.

--- scripts/import/GHS_POP.sh
+++ scripts/import/GHS_POP.sh
@@ -2,10 +2,10 @@
 #
 # http://cidportal.jrc.ec.europa.eu/ftp/jrc-opendata/GHSL/GHS_POP_MT_GLOBE_R2019A/GHS_POP_E2015_GLOBE_R2019A_54009_250/V1-0/GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0.zip
 #
-# '2>> error-guf04.txt':
+# '2>> error.txt':
 # The standard error stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
 
-geotiff=data/GHS_POP/GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0.tif
+geotiff=GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0.tif
 
 raster2pgsql \
     -I \
@@ -14,20 +14,30 @@ raster2pgsql \
     -c \
     -t 100x100 \
     -s 954009 \
-    $geotiff public.ghspop \
-    2>> error-ghspop.log \
+    $geotiff public.ghs_pop \
+    2>> error-ghs_pop.log \
     | \
     psql \
         -h localhost \
         -p 5432 \
         -d hexadmin \
         -U hexadmin \
-        2>> error-ghspop.log
+        2>> error-ghs_pop.log
 
 psql \
     -h localhost \
     -p 5432 \
     -d hexadmin \
     -U hexadmin \
-    -c "SELECT AddRasterConstraints('public.ghspop'::name, 'rast'::name);" \
-    2>> error-ghspop.log
+    -c "SELECT AddRasterConstraints('public.ghs_pop'::name, 'rast'::name);" \
+    2>> error-ghs_pop.log
+
+# After import of raster srid is: 954009
+# Postgis nows only equivalent srid: 54009
+psql \
+    -h localhost \
+    -p 5432 \
+    -d hexadmin \
+    -U hexadmin \
+    -c "SELECT UpdateRasterSRID('ghs_pop', 'rast', 54009);" \
+    2>> error-ghs_pop.log
