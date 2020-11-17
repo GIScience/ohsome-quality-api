31b07f7398fc07fcc192d39d91e836fdcf8f6acd
matthias_schaub <matthias.schaub@posteo.de>
2020-11-17 15:40:41 +0100

    Remove update raster srid to 54009.

--- scripts/import/GHS_POP.sh
+++ scripts/import/GHS_POP.sh
@@ -7,6 +7,14 @@
 
 geotiff=GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0.tif
 
+psql \
+    -h localhost \
+    -p 5432 \
+    -d hexadmin \
+    -U hexadmin \
+    -c "INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 954009, 'esri', 54009, '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ', 'PROJCS["World_Mollweide",GEOGCS["GCS_WGS_1984",DATUM["WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Mollweide"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["Central_Meridian",0],UNIT["Meter",1],AUTHORITY["EPSG","54009"]]');"
+    2>> error-ghs_pop.log
+
 raster2pgsql \
     -I \
     -M \
@@ -30,14 +38,4 @@ psql \
     -d hexadmin \
     -U hexadmin \
     -c "SELECT AddRasterConstraints('public.ghs_pop'::name, 'rast'::name);" \
-    2>> error-ghs_pop.log
-
-# After import of raster srid is: 954009
-# Postgis nows only equivalent srid: 54009
-psql \
-    -h localhost \
-    -p 5432 \
-    -d hexadmin \
-    -U hexadmin \
-    -c "SELECT UpdateRasterSRID('ghs_pop', 'rast', 54009);" \
     2>> error-ghs_pop.log
