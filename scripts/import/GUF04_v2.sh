170be5822ade0eba790641dc162f2c00980a152a
matthias_schaub <matthias.schaub@posteo.de>
2020-11-23 16:43:21 +0100

    Change nodata values of GUF04 before import.

--- scripts/import/GUF04_v2.sh
+++ scripts/import/GUF04_v2.sh
@@ -3,6 +3,8 @@
 # '2>> error-guf04.txt':
 # The standard error stream will be redirected to the file only, it will not be visible in the terminal. If the file already exists, the new data will get appended to the end of the file.
 
-raster2pgsql -c -M -I -t 100x100 -s 4326 *.tif public.guf04 2>> error-guf04.txt | psql -U postgres -d pop 2>> error-guf04.txt
+gdalbuildvrt -vrtnodata "0 128" guf04.vrt data/GUF04_v2/*.tif
 
-psql -U postgres -d pop -c 'SELECT AddRasterConstraints('guf04'::name, 'rast'::name);' # AddRasterConstraints
+raster2pgsql -c -M -I -C -t 100x100 -s 4326 data/GUF04_v2/guf04.vrt public.guf04 2>> error-guf04.txt | PGPASSWORD=mypassword psql -h localhost -p 5445 -U hexadmin -d hexadmin 2>> error-guf04.txt
+
+psql -h localhost -p 5445 -U hexadmin -d hexadmin -c 'SELECT AddRasterConstraints('guf04'::name, 'rast'::name);' # AddRasterConstraints
