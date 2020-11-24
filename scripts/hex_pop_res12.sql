930b5b9d810d22cfca5d99a1f567997ecfb40c49
matthias_schaub <matthias.schaub@posteo.de>
2020-11-24 16:00:50 +0100

    Add sql queries for aggregation of ghs pop at hex grid res 6 and 12.

---
+++ scripts/hex_pop_res12.sql
@@ -0,0 +1,35 @@
+ALTER TABLE isea3h_world_res_12_hex
+    ADD COLUMN IF NOT EXISTS population FLOAT;
+
+WITH hex_pop AS (
+    SELECT
+        geohash_id,
+        (stats).sum AS population
+        -- (stats).count AS pixel_count,
+        -- (stats).count * 0.25 * 0.25 AS area_sqkm,
+        -- (stats).sum / ((stats).count * 0.25 * 0.25) AS pop_per_sqkm
+    FROM (
+        SELECT
+            geohash_id,
+            -- it's important do to the union here and group by rid
+            -- otherwise we might count some pixel several times
+            ST_SummaryStats (ST_Union (ST_Clip (rast, ST_Transform (geom, 954009)))) AS stats
+    FROM
+        ghs_pop,
+        isea3h_world_res_12_hex
+    WHERE
+        ST_Intersects (rast, ST_Transform (geom, 954009))
+        -- Ignore grid cells at the very edge of the globe to avoid following ERROR:
+        -- rt_raster_new: Dimensions requested exceed the maximum permitted for a raster.
+        AND abs(ST_xMin (ST_Transform (geom, 954009)) - ST_xMax (ST_Transform (geom, 954009))) < 100000
+        AND abs(ST_yMin (ST_Transform (geom, 954009)) - ST_yMax (ST_Transform (geom, 954009))) < 100000
+    GROUP BY
+        geohash_id) AS summary_stats)
+UPDATE
+    isea3h_world_res_12_hex AS hex
+SET
+    population = hex_pop.population
+FROM
+    hex_pop
+WHERE
+    hex.geohash_id = hex_pop.geohash_id;
