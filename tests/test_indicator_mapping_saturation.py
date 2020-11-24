d6446cc14a4011d73b927d1fb020f2e3257475a3
h9b <herfort@uni-heidelberg.de>
2020-11-24 18:24:49 +0100

    add test

---
+++ tests/test_indicator_mapping_saturation.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+
+from ohsome_quality_tool.oqt import get_dynamic_indicator
+from ohsome_quality_tool.utils.definitions import Indicators
+
+
+class TestMappingSaturationIndicator(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = os.path.dirname(os.path.abspath(__file__))
+        self.indicator_name = Indicators.MAPPING_SATURATION._name_
+
+    def test_get_dynamic_indicator(self):
+        """Test if dynamic indicator can be calculated."""
+        infile = os.path.join(self.test_dir, "fixtures/antanarivo.geojson")
+        results = get_dynamic_indicator(
+            indicator_name=self.indicator_name, infile=infile
+        )
+
+        # check if result dict contains the right keys
+        self.assertListEqual(
+            list(results.keys()), ["data", "quality_level", "description"]
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
