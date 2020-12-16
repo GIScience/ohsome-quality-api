7d53f9ecc2991e5b12ebd627c1cfe2b61263e83f
Katharina Przybill <katharina.przybill@heigit.org>
2020-12-16 11:41:33 +0000

    add MAPPING_SATURATION_LABEL_INTERPRETATIONS

--- ohsome_quality_tool/utils/label_interpretations.py
+++ ohsome_quality_tool/utils/label_interpretations.py
@@ -12,3 +12,10 @@ POI_DENSITY_LABEL_INTERPRETATIONS = \
          red="It is probably hard to orientate on OSM-based sketchmaps of this region. "
              "There are just few orientation providing features available, you should explore, "
              "if participants can orientate properly.")
+
+MAPPING_SATURATION_LABEL_INTERPRETATIONS = \
+    dict(green="Saturation reached. The data in this region seem quite saturated "
+               "with a growth of data less than 3 % within the last 3 years.",
+         yellow="Saturation not yet reached. The data in this region still seem to be in a growth stadium  "
+               "with a growth of data more than 3 % within the last 3 years.",
+         red="No Saturation at all. The data in this region are in a start stadium."
