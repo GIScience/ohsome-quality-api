b3e419baf4805685ff3cc46e413dc33f5d5d42bc
Katharina Przybill <katharina.przybill@heigit.org>
2020-12-16 12:42:32 +0000

    Add some words to quality of completeness in mapping saturation description

--- ohsome_quality_tool/utils/label_interpretations.py
+++ ohsome_quality_tool/utils/label_interpretations.py
@@ -15,7 +15,12 @@ POI_DENSITY_LABEL_INTERPRETATIONS = \
 
 MAPPING_SATURATION_LABEL_INTERPRETATIONS = \
     dict(green="Saturation reached. The data in this region seem quite saturated "
-               "with a growth of data less than 3 % within the last 3 years.",
+               "with a growth of data less than 3 % within the last 3 years. " 
+               "This indicates good quality in respect to completeness.",
          yellow="Saturation not yet reached. The data in this region still seem to be in a growth stadium  "
-               "with a growth of data more than 3 % within the last 3 years.",
-         red="No Saturation at all. The data in this region are in a start stadium."
+               "with a growth of data more than 3 % within the last 3 years. "
+               "This indicates a difficult to sepcify quality in respect to completeness, "
+               "because it depends on the growth rate over time. The growth could be "
+               "near to saturated or still be in an high growth stadium.",
+         red="No Saturation at all. The data in this region are in a start stadium. With regard "
+               "to completeness, this means bad quality."
