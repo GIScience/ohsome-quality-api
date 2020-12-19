b6ee32fd707ba16c74c5f708dac511ca8081d166
h9b <herfort@uni-heidelberg.de>
2020-12-19 01:23:53 +0100

    add label interpreations for remote mapping report

--- ohsome_quality_tool/reports/remote_mapping_level_one/report.py
+++ ohsome_quality_tool/reports/remote_mapping_level_one/report.py
@@ -1,3 +1,4 @@
+from statistics import mean
 from typing import Dict
 
 from geojson import FeatureCollection
@@ -15,10 +16,27 @@ class Report(BaseReport):
     """The remote mapping level one Report."""
 
     name = "remote-mapping-level-one"
-    description = """
-        This report shows the quality for map features that are usually
-        added on the basis of satellite imagery.
-    """
+    description = (
+        "This report shows the quality for map features that are usually "
+        "added on the basis of satellite imagery. It combines five indicators "
+        "to inspect the quality in regard to buildings and major roads."
+    )
+    interpretations: Dict = {
+        "green": (
+            "All indicators show a good quality. "
+            "The data in this regions seems to be completely mapped."
+        ),
+        "yellow": (
+            "At least one indicator shows only medium quality. "
+            "You should inspect the results for the individual indicators "
+            "to identify potential data quality concerns."
+        ),
+        "red": (
+            "The data quality in this region is low. "
+            "It is very likely that this regions has not been completely "
+            "mapped in OSM."
+        ),
+    }
 
     indicator_classes: Dict = get_indicator_classes()
     indicators_definition = [
@@ -46,7 +64,25 @@ class Report(BaseReport):
         """Combine the individual scores per indicator."""
         logger.info(f"combine indicators for {self.name} report.")
 
-        result = ReportResult(
-            label=TrafficLightQualityLevels.YELLOW, value=0.5, text="test test test"
-        )
-        return result
+        # get mean of indicator quality values
+        values = []
+
+        for indicator in indicators:
+            values.append(indicator["result"]["value"])
+
+        value = mean(values)
+
+        if value < 0.5:
+            label = TrafficLightQualityLevels.RED
+            text = self.interpretations["red"]
+        elif value < 1:
+            label = TrafficLightQualityLevels.YELLOW
+            text = self.interpretations["yellow"]
+        elif value >= 1:
+            label = TrafficLightQualityLevels.GREEN
+            text = self.interpretations["green"]
+        else:
+            label = None
+            text = "Could not derive quality level"
+
+        return label, value, text
