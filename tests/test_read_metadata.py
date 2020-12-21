105bf29b07617256a6702fcf353e780022e7e6be
Matthias Schaub <matthias.schaub@posteo.de>
2020-12-21 14:47:00 +0100

    test read metadata function (wip)

--- tests/test_read_metadata.py
+++ tests/test_read_metadata.py
@@ -1,20 +1,30 @@
 import unittest
 
+from schema import Schema
+
 from ohsome_quality_tool.indicators.guf_comparison.indicator import Indicator
 
 
 class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        self.metadata = Indicator(dynamic=True, bpolys="").read_metadata()
+        self.schema = Schema(
+            [{"id": {"name": str, "description": str, "label_interpretation": dict}}]
+        )
+
     def test_upper(self):
         # TODO: Test if dict is of schema.
-        indicator = Indicator(dynamic=True, bpolys="")
-        result = indicator.read_metadata()
+        print(self.metadata)
 
-        self.assertIsInstance(result, dict)
-        self.assertEqual(len(result.keys()), 1)
+        self.assertIsInstance(self.metadata, dict)
+        self.assertEqual(len(self.metadata.keys()), 1)
 
-        for key, value in result.items():
+        for key, value in self.metadata.items():
             for k in ("name", "description", "label_interpretation"):
                 self.assertIn(k, value.keys())
+
+    def validate_schema(self):
+        self.schema.is_valid(self.metadata)
 
 
 if __name__ == "__main__":
