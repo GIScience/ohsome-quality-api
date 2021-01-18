673a36690ad38c9f525be31e7e3668b677a6241d
Matthias Schaub <matthias.schaub@posteo.de>
2021-01-18 15:22:13 +0100

    Rewrite to use new base class structure.

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Utils
 python -m unittest test_logging.py
 python -m unittest test_load_layers.py
@@ -5,3 +7,5 @@ python -m unittest test_load_metadata.py
 
 # Indicators
 python -m unittest test_indicator_ghspop.py
+python -m unittest test_indicator_last_edit_2.py
+python -m unittest test_indicator_poi_density_2.py
