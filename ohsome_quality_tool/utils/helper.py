1bb76fc8c657c68086f9a0ef5aa95eff3d6ff465
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2021-01-22 15:26:30 +0100

    name_to_class can now handle report and indicator classes.

--- ohsome_quality_tool/utils/helper.py
+++ ohsome_quality_tool/utils/helper.py
@@ -8,8 +8,8 @@ import pkgutil
 import re
 
 
-def name_to_class(class_name: str):
-    """Convert class name of indicator to the class.
+def name_to_class(class_type: str, name: str):
+    """Convert class name of class type (indicator or report) to the class.
 
     Assumptions:
     - Class is named in Camel Case (E.g. GhsPopComparison).
@@ -19,11 +19,12 @@ def name_to_class(class_name: str):
     # - Hard code import of classes
     # - Dynamically import all classes in package
     #     - https://julienharbulot.com/python-dynamical-import.html
-
-    class_path = "ohsome_quality_tool.indicators.{0}.indicator".format(
-        camel_to_snake(class_name)
+    class_path = "ohsome_quality_tool.{0}s.{1}.{2}".format(
+        class_type,
+        camel_to_snake(name),
+        class_type,
     )
-    return getattr(importlib.import_module(class_path), class_name)
+    return getattr(importlib.import_module(class_path), name)
 
 
 def camel_to_snake(string: str) -> str:
--- tests/test_name_to_class.py
+++ tests/test_name_to_class.py
@@ -1,21 +1,32 @@
 import unittest
 
 from ohsome_quality_tool.indicators.ghs_pop_comparison.indicator import GhsPopComparison
+
+# from ohsome_quality_tool.reports.simple_report.report import SimpleReport
 from ohsome_quality_tool.utils.definitions import load_indicator_metadata
 from ohsome_quality_tool.utils.helper import name_to_class
 
 
 class TestNameToClass(unittest.TestCase):
     def setUp(self):
-        self.class_name = "GhsPopComparison"
         self.indicators = load_indicator_metadata()
 
     def test(self):
-        self.assertIs(name_to_class(self.class_name), GhsPopComparison)
+        self.assertIs(
+            name_to_class(class_type="indicator", name="GhsPopComparison"),
+            GhsPopComparison,
+        )
+        # TODO
+        # self.assertIs(
+        #     name_to_class(class_type="report", name="SimpleReport"),
+        #     SimpleReport,
+        # )
 
     def test_all_indicators(self):
         for indicator in self.indicators.keys():
-            self.assertIsNotNone(name_to_class(indicator))
+            self.assertIsNotNone(
+                name_to_class(class_type="indicator", name="GhsPopComparison")
+            )
 
 
 if __name__ == "__main__":
