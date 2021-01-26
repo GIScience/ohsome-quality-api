6a438194a1c22323dc89b5487ac647ac11fe8cf8
Matthias_Schaub <schaub@stud.uni-heidelberg.de>
2021-01-26 15:24:28 +0100

    Rewrite to match new report base class.

--- ohsome_quality_tool/reports/remote_mapping_level_one/report.py
+++ ohsome_quality_tool/reports/remote_mapping_level_one/report.py
@@ -3,15 +3,11 @@ from statistics import mean
 from geojson import FeatureCollection
 
 from ohsome_quality_tool.base.report import BaseReport
-from ohsome_quality_tool.utils.definitions import (
-    ReportResult,
-    TrafficLightQualityLevels,
-    logger,
-)
-from ohsome_quality_tool.utils.helter import name_to_class
+from ohsome_quality_tool.utils.definitions import TrafficLightQualityLevels, logger
+from ohsome_quality_tool.utils.helper import name_to_class
 
 
-class RemoteMappingLevel(BaseReport):
+class RemoteMappingLevelOne(BaseReport):
     """The remote mapping level one Report."""
 
     def __init__(
@@ -20,39 +16,41 @@ class RemoteMappingLevel(BaseReport):
         dataset: str = None,
         feature_id: int = None,
     ) -> None:
+        """Create a list of indicator objects."""
+
         super().__init__(bpolys=bpolys, dataset=dataset, feature_id=feature_id)
         for indicator_name, layer_name in (
             # TODO ("GufComparison"], "building-area"),
-            ("GhsPopComparison", "building-count"),
-            ("MappingSaturation", "building-count"),
-            ("LastEdit", "building-count"),
-            ("MappingSaturation", "major-roads"),
-            ("LastEdit", "major-roads"),
+            # ("MappingSaturation", "building_count"),
+            # ("MappingSaturation", "major_roads"),
+            ("GhsPopComparison", "building_count"),
+            ("LastEdit", "building_count"),
+            ("LastEdit", "major_roads"),
         ):
             indicator_class = name_to_class(class_type="indicator", name=indicator_name)
             indicator = indicator_class(layer_name=layer_name, bpolys=bpolys)
             self.indicators.append(indicator)
 
-    def combine(self, indicators) -> ReportResult:
-        """Combine the individual scores per indicator."""
+    def combine(self):
+        """Combine the results of all indicators."""
         logger.info(f"Combine indicators for report: {self.metadata.name}")
 
         values = []
         for indicator in self.indicators:
-            # TODO: When is label == UNDEFINED?
+            # TODO: Is it possible that a label == UNDEFINED?
             if indicator.result.label != "UNDEFINED":
                 values.append(indicator.result.value)
         self.result.value = mean(values)
 
         if self.result.value < 0.5:
             self.result.label = TrafficLightQualityLevels.RED
-            self.result.description = self.interpretations["red"]
+            self.result.description = self.metadata.label_description["red"]
         elif self.result.value < 1:
             self.result.label = TrafficLightQualityLevels.YELLOW
-            self.result.description = self.interpretations["yellow"]
+            self.result.description = self.metadata.label_description["yellow"]
         elif self.result.value >= 1:
             self.result.label = TrafficLightQualityLevels.GREEN
-            self.result.description = self.interpretations["green"]
+            self.result.description = self.metadata.label_description["green"]
         else:
             self.result.label = None
             self.result.description = "Could not derive quality level"
