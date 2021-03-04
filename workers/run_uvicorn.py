d0d057f43b5b3fc0071bb21db1c01fc2786b0b2b
Matthias Schaub <matthias.schaub@heigit.org>
2021-03-04 12:05:35 +0100

    Update function name and change port number
    Update function name of get_log_config.
    Change port number to be the same as in docker-compose.development.yml

--- workers/run_uvicorn.py
+++ workers/run_uvicorn.py
@@ -1,11 +1,12 @@
 import uvicorn
 
-from ohsome_quality_analyst.utils.definitions import get_log_config
+from ohsome_quality_analyst.utils.definitions import load_logging_config
 
 if __name__ == "__main__":
     uvicorn.run(
         "ohsome_quality_analyst.api:app",
+        port=8080,
         reload=True,
         reload_dirs=["."],
-        log_config=get_log_config(),
+        log_config=load_logging_config(),
     )
