47c2a2164c1f0ca87465a7d968a9d89732164fbd
Katharina Przybill <przybill@hce.uni-heidelberg.de>
2021-04-07 18:38:29 +0200

    remove print out

--- workers/ohsome_quality_analyst/indicators/tags_ratio/indicator.py
+++ workers/ohsome_quality_analyst/indicators/tags_ratio/indicator.py
@@ -41,16 +41,6 @@ class TagsRatio(BaseIndicator):
         self.ratio = query_results_count["ratioResult"][0]["ratio"]
         self.count_all = query_results_count["ratioResult"][0]["value"]
         self.count_match = query_results_count["ratioResult"][0]["value2"]
-        print(
-            str(self.feature_id)
-            + "; "
-            + str(self.ratio)
-            + "; "
-            + str(self.count_all)
-            + "; "
-            + str(self.count_match)
-            + "; "
-        )
 
     def calculate(self) -> bool:
 
@@ -108,7 +98,7 @@ class TagsRatio(BaseIndicator):
         figsize = (400 * px, 400 * px)
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot()
-        if self.ratio is None:
+        """if self.ratio is None:
             ax.set_title("Could not calculate indicator")
             img_data = StringIO()
             plt.savefig(img_data, format="svg")
@@ -122,64 +112,63 @@ class TagsRatio(BaseIndicator):
             self.result.svg = img_data.getvalue()
             logging.info(f"Got svg-figure string for indicator {self.metadata.name}")
             plt.close("all")
+        else:"""
+        ax.set_title(
+            "Ratio between all features ("
+            + str(self.count_all)
+            + ")"
+            + "\nand filtered ones ("
+            + str(self.count_match)
+            + ")"
+        )
+
+        size = 0.3  # Width of the pie
+        handles = []  # Handles for legend
+
+        # Plot outer Pie (Traffic Light)
+        radius = 1
+        sizes = [0.25, 0.50, 0.25]
+        colors = ["green", "yellow", "red"]
+        labels = ["Good", "Medium", "Bad"]
+        ax.pie(
+            sizes,
+            radius=radius,
+            colors=colors,
+            startangle=90,
+            wedgeprops={"width": size, "alpha": 0.5},
+        )
+
+        for c, s, l in zip(colors, sizes, labels):
+            handles.append(mpatches.Patch(color=c, label=f"{l}"))
+
+        # Plot inner Pie (Indicator Value)
+        radius = 1 - size
+        if type(self.ratio) == str:
+            sizes = (1 - 1, 1)
         else:
-            ax.set_title(
-                "Ratio between all features ("
-                + str(self.count_all)
-                + ")"
-                + "\nand filtered ones ("
-                + str(self.count_match)
-                + ")"
-            )
+            sizes = (1 - self.ratio, self.ratio)
+        colors = ("white", "black")
+        ax.pie(
+            sizes,
+            radius=radius,
+            colors=colors,
+            startangle=90,
+            wedgeprops={"width": size},
+        )
 
-            size = 0.3  # Width of the pie
-            handles = []  # Handles for legend
+        black_patch = mpatches.Patch(
+            color="black",
+            label=f"{self.layer.name} \nRatio: " f"{round(self.ratio, 2)}",
+        )
+        handles.append(black_patch)
 
-            # Plot outer Pie (Traffic Light)
-            radius = 1
-            sizes = [0.25, 0.50, 0.25]
-            colors = ["green", "yellow", "red"]
-            labels = ["Good", "Medium", "Bad"]
-            ax.pie(
-                sizes,
-                radius=radius,
-                colors=colors,
-                startangle=90,
-                wedgeprops={"width": size, "alpha": 0.5},
-            )
+        ax.legend(handles=handles)
+        ax.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-            for c, s, l in zip(colors, sizes, labels):
-                handles.append(mpatches.Patch(color=c, label=f"{l}"))
-
-            # Plot inner Pie (Indicator Value)
-            radius = 1 - size
-            if type(self.ratio) == str:
-                sizes = (1 - 1, 1)
-            else:
-                sizes = (1 - self.ratio, self.ratio)
-            colors = ("white", "black")
-            ax.pie(
-                sizes,
-                radius=radius,
-                colors=colors,
-                startangle=90,
-                wedgeprops={"width": size},
-            )
-
-            black_patch = mpatches.Patch(
-                color="black",
-                label=f"{self.layer.name} \nRatio: " f"{round(self.ratio, 2)}",
-            )
-            handles.append(black_patch)
-
-            ax.legend(handles=handles)
-            ax.axis(
-                "equal"
-            )  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-            img_data = StringIO()
-            plt.savefig(img_data, format="svg", bbox_inches="tight")
-            self.result.svg = img_data.getvalue()
-            logging.info(f"Got svg-figure string for indicator {self.metadata.name}")
-            plt.close("all")
-            return True
+        img_data = StringIO()
+        plt.savefig(img_data, format="svg", bbox_inches="tight")
+        self.result.svg = img_data.getvalue()
+        plt.savefig("test_mappingSat_jrc.png", format="png", bbox_inches="tight")
+        logging.info(f"Got svg-figure string for indicator {self.metadata.name}")
+        plt.close("all")
+        return True
