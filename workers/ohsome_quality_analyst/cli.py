f707246abbb117a6cd2a85eb0c82719def86dc82
Danner, Jonas <danner@stud.uni-heidelberg.de>
2021-05-05 13:32:50 +0200

    combined 2 lines into 1

--- workers/ohsome_quality_analyst/cli.py
+++ workers/ohsome_quality_analyst/cli.py
@@ -112,12 +112,11 @@ def create_indicator(
         if bpolys.is_valid is False:
             raise ValueError("Input geometry is not valid")
         for i in range(len(bpolys.features)):
-            a = bpolys.features[i]
-            b = geojson.FeatureCollection([a])
+            bpolys_subset = geojson.FeatureCollection([bpolys.features[i]])
             indicator = asyncio.run(
                 oqt.create_indicator(
                     indicator_name=indicator_name,
-                    bpolys=b,
+                    bpolys=bpolys_subset,
                     layer_name=layer_name,
                     feature_id=feature_id,
                     dataset=dataset_name,
