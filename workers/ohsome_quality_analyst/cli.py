f3357830bcf2bda957099095b9381cd4de43e819
Danner, Jonas <danner@stud.uni-heidelberg.de>
2021-05-05 13:56:13 +0200

    added the return a GeoJSON with the result object of an indictor as property when computing an indicator from the CLI

--- workers/ohsome_quality_analyst/cli.py
+++ workers/ohsome_quality_analyst/cli.py
@@ -127,6 +127,14 @@ def create_indicator(
             r = vars(indicator.result)
             bpolys["features"][i]["properties"].update(m)
             bpolys["features"][i]["properties"].update(r)
+        try:
+            outputfile = infile[:-8] + "_%s.geojson" % indicator_name
+            with open(outputfile, "w") as f:
+                geojson.dump(bpolys, f)
+        except Exception as err:
+            logging.error(
+                "could not write outputfile %s. Error: %s" % (outputfile, err)
+            )
     else:
         bpolys = None
         indicator = asyncio.run(
