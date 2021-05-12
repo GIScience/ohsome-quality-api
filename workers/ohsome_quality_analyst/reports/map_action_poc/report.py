2540c2fc5b503ad19a7c842e6e5690e1dadad5c4
Matthias Schaub <matthias.schaub@heigit.org>
2021-05-12 15:07:48 +0200

    Rename report class to avoid path finding issues

--- workers/ohsome_quality_analyst/reports/map_action_poc/report.py
+++ workers/ohsome_quality_analyst/reports/map_action_poc/report.py
@@ -6,7 +6,7 @@ from geojson import FeatureCollection
 from ohsome_quality_analyst.base.report import BaseReport, IndicatorLayer
 
 
-class MapActionPOC(BaseReport):
+class MapActionPoc(BaseReport):
     def __init__(
         self,
         bpolys: FeatureCollection = None,
