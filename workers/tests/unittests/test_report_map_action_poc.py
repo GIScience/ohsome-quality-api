9a318e8140bae9e02c7a298be0bba06409dadb7e
Daniel De Barros Soares <ddebarros@mapaction.org>
2021-05-13 15:15:59 +0000

    Improve report tests by using mocks to avoid querying ohsome API

---
+++ workers/tests/unittests/test_report_map_action_poc.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import Mock
+
+from ohsome_quality_analyst.reports.map_action_poc.report import MapActionPoc
+
+
+class TestReportMapActionPoc(unittest.TestCase):
+    def test_combine_indicators_mean(self):
+        report = MapActionPoc()
+        report.set_indicator_layer()
+
+        # Mock indicator objects with a fixed result value
+        for _ in report.indicator_layer:
+            indicator = Mock()
+            indicator.result = Mock()
+            indicator.result.value = 0.5
+            report.indicators.append(indicator)
+
+        report.combine_indicators()
+
+        self.assertIsNotNone(report.result.label)
+        self.assertIsNotNone(report.result.description)
+        # Should be the mean of all indicator result values
+        self.assertEqual(report.result.value, 0.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
