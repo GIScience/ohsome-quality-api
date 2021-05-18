be2ed67f6f7bb03f8e89f404d5beabb07baad878
Matthias Schaub <matthias.schaub@heigit.org>
2021-05-18 09:44:41 +0200

    Fix wrong layer name for Map Action Report

--- workers/ohsome_quality_analyst/reports/map_action_poc/report.py
+++ workers/ohsome_quality_analyst/reports/map_action_poc/report.py
@@ -21,10 +21,10 @@ class MapActionPoc(BaseReport):
         self.indicator_layer = (
             IndicatorLayer("MappingSaturation", "mapaction_settlements_count"),
             IndicatorLayer("MappingSaturation", "mapaction_capital_city_count"),
-            IndicatorLayer("MappingSaturation", "major_roads"),
+            IndicatorLayer("MappingSaturation", "major_roads_length"),
             IndicatorLayer("LastEdit", "mapaction_settlements_count"),
             IndicatorLayer("LastEdit", "mapaction_capital_city_count"),
-            IndicatorLayer("LastEdit", "major_roads"),
+            IndicatorLayer("LastEdit", "major_roads_length"),
             IndicatorLayer("LastEdit", "mapaction_lakes_count"),
             IndicatorLayer("LastEdit", "mapaction_rivers_length"),
             IndicatorLayer("LastEdit", "mapaction_rail_length"),
