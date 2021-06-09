8bb4f0c0ad9c318c7963d02e2846699f83a81f1b
Matthias Schaub <83658582+matthiasschaub@users.noreply.github.com>
2021-06-09 09:22:22 +0200

    Raise exceptions in ohsome client instead of returning None in case of failed ohsome API query (#29)
    * Raise exceptions instead of returning None
    
    Raise exceptions in ohsome client instead of returning None in case of failed ohsome API query.
    
    * Recreate VCR cassette
    
    * Substitute removesuffix with rstrip
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    
    * Add build_data_dict function to ohsome client
    
    * Add unittests for build_data_dict of ohsome client
    
    * Improve comment
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    
    * Remove duplicated log message
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    
    * Add unittests for build_url function
    
    * Add custom exception for ohsome client
    
    Improve error message by adding a custom Exception for the ohsome client
    module and additional context in text form.
    
    * Minor spelling fix
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    
    * Minor changes to comments and names
    
    * Remove check and tests for time parameter
    
    * Improve OhsomeApiError by adding msg of resp
    
    * Update Changelog
    
    * Reset VCR cassette to version in main
    
    * Revert removing empty line
    
    * Remove dependency on env for ohsome client tests
    
    * Add comment on time parameter validation
    
    * Add test for invalid ohsome api response
    
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>
    Co-authored-by: Matthias Schaub <matthias.schaub@posteo.de>

--- workers/ohsome_quality_analyst/ohsome/client.py
+++ workers/ohsome_quality_analyst/ohsome/client.py
@@ -8,10 +8,10 @@ import geojson
 import httpx
 
 from ohsome_quality_analyst.utils.definitions import OHSOME_API
+from ohsome_quality_analyst.utils.exceptions import OhsomeApiError
 
 
-# TODO: Add documentation on time string format.
-# TODO: Add tests for ohsome package, including time = None leading to errors
+# TODO: Add more tests for ohsome package.
 async def query(
     layer,
     bpolys: str,
@@ -19,46 +19,44 @@ async def query(
     endpoint: Optional[str] = None,
     ratio: bool = False,
 ) -> Optional[Dict]:
-    """Query ohsome API endpoint with filter."""
-    if endpoint is not None:
-        url = OHSOME_API + endpoint
-    else:
-        url = OHSOME_API + layer.endpoint
+    """
+    Query ohsome API endpoint with filter.
 
-    if ratio:
-        url = url + "/ratio"
-        data = {"bpolys": bpolys, "filter": layer.filter, "filter2": layer.ratio_filter}
-    else:
-        data = {"bpolys": bpolys, "filter": layer.filter}
-
-    if time is not None:
-        data["time"] = time
-
+    Time is one or more ISO-8601 conform timestring(s).
+    https://docs.ohsome.org/ohsome-api/v1/time.html
+    """
+    url = build_url(layer, endpoint, ratio)
+    data = build_data_dict(layer, bpolys, time, ratio)
     logging.info("Query ohsome API.")
-    logging.debug("Query data: " + json.dumps(data, indent=4, sort_keys=True))
+    logging.debug("Query URL: " + url)
+    logging.debug("Query data: " + json.dumps(data))
+    return await query_ohsome_api(url, data)
 
-    # set custom timeout as ohsome API takes a long time to send an answer
-    timeout = httpx.Timeout(5, read=600)
+
+async def query_ohsome_api(url: str, data: dict):
+    # custom timeout as ohsome API can take a long time to send an answer
+    # (up to 10 minutes)
+    timeout = httpx.Timeout(5, read=660)
     async with httpx.AsyncClient(timeout=timeout) as client:
         resp = await client.post(url, data=data)
 
-    logging.info("Query ohsome API.")
-    logging.debug("Query URL: " + url)
-    logging.debug("Query Filter: " + layer.filter)
-    if resp.status_code == 200:
-        try:
-            logging.info("Ohsome query successful!")
-            logging.debug(
-                "Query response: " + json.dumps(resp.json(), indent=4, sort_keys=True)
-            )
-            return geojson.loads(resp.content)
-        except JSONDecodeError:
-            # ohsome API can return broken GeoJSON during streaming of response
-            logging.warning("Ohsome query failed!")
-            return None
-    elif resp.status_code == 404:
-        logging.warning("Query failed!")
-        return None
+    # ohsome API response status codes are either 4xx and 5xx or 200
+    try:
+        # Raise for response status codes 4xx and 5xx.
+        # This will raise an error (400) in case of invalid time parameter.
+        resp.raise_for_status()
+    except httpx.HTTPStatusError as error:
+        raise OhsomeApiError(
+            "Querying the ohsome API failed!" + error.response.json()["message"]
+        ) from error
+
+    try:
+        return geojson.loads(resp.content)
+    except JSONDecodeError as error:
+        raise OhsomeApiError(
+            "Ohsome API returned invalid GeoJSON after streaming of the response. "
+            + "The reason is a timeout of the ohsome API."
+        ) from error
 
 
 async def get_latest_ohsome_timestamp():
@@ -69,3 +67,38 @@ async def get_latest_ohsome_timestamp():
     timestamp_str = str(resp.json()["extractRegion"]["temporalExtent"]["toTimestamp"])
     timestamp = datetime.datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%MZ")
     return timestamp
+
+
+def build_url(
+    layer,
+    endpoint: Optional[str] = None,
+    ratio: bool = False,
+) -> str:
+    """Build endpoint URL of ohsome API."""
+    ohsome_api = OHSOME_API.rstrip("/")
+    if endpoint is None:
+        endpoint = layer.endpoint
+    url = ohsome_api + "/" + endpoint.rstrip("/")
+    if ratio:
+        return url + "/" + "ratio"
+    return url
+
+
+def build_data_dict(
+    layer,
+    bpolys: str,
+    time: Optional[str] = None,
+    ratio: bool = False,
+) -> dict:
+    """Build data dictionary for ohsome API query."""
+    data = {"bpolys": bpolys, "filter": layer.filter}
+    if ratio:
+        if layer.ratio_filter is None:
+            raise ValueError(
+                "Layer '{0}' has not 'ratio_filter' defined.".format(layer.name)
+            )
+        else:
+            data["filter2"] = layer.ratio_filter
+    if time is not None:
+        data["time"] = time
+    return data
