a7c0343660a1aea7fb84a9b7f11dc545813137a6
Matthias Schaub <matthias.schaub@heigit.org>
2021-06-28 13:39:40 +0200

    Use ohsome API endpoint contributions/latest/count

--- workers/ohsome_quality_analyst/indicators/last_edit/indicator.py
+++ workers/ohsome_quality_analyst/indicators/last_edit/indicator.py
@@ -13,6 +13,8 @@ from ohsome_quality_analyst.ohsome import client as ohsome_client
 
 
 class LastEdit(BaseIndicator):
+    """Ratio of latest contribution count to element count."""
+
     def __init__(
         self,
         layer_name: str,
@@ -23,83 +25,79 @@ class LastEdit(BaseIndicator):
             layer_name=layer_name,
             bpolys=bpolys,
         )
-        self.time_range = time_range
-        # TODO: thresholds might be better defined for each OSM layer
         self.threshold_yellow = 20  # more than 20% edited last year --> green
-        self.threshold_red = 5  # more than 5% edited last year --> yellow
-        self.edited_features = None
-        self.total_features = None
-        self.share_edited_features = None
+        self.threshold_red = 5
+
+        self.time_range = time_range
+        self.element_count = None
+        self.contributions_latest_count = None
+        self.ratio = None  # Ratio of latest contribution count to element count
 
     async def preprocess(self) -> bool:
         if self.time_range is None:
             latest_ohsome_stamp = await ohsome_client.get_latest_ohsome_timestamp()
-            self.time_range = "{},{}".format(
-                (latest_ohsome_stamp - relativedelta(years=1)).strftime("%Y-%m-%d"),
-                latest_ohsome_stamp.strftime("%Y-%m-%d"),
+            self.time_range = "{start},{end}".format(
+                start=(latest_ohsome_stamp - relativedelta(years=1)).strftime(
+                    "%Y-%m-%d"
+                ),
+                end=latest_ohsome_stamp.strftime("%Y-%m-%d"),
             )
 
-        query_results_contributions = await ohsome_client.query(
+        _ = await ohsome_client.query(
             layer=self.layer,
             bpolys=json.dumps(self.bpolys),
             time=self.time_range,
-            endpoint="contributions/latest/centroid",
+            endpoint="contributions/latest/count",
         )
-        query_results_totals = await ohsome_client.query(
+        self.contributions_latest_count = _["result"][0]["value"]
+        _ = await ohsome_client.query(
             layer=self.layer,
             bpolys=json.dumps(self.bpolys),
         )
-        if query_results_contributions is None or query_results_totals is None:
-            return False
-        try:
-            self.edited_features = len(query_results_contributions["features"])
-        except KeyError:
-            # no feature has been edited in the time range
-            self.edited_features = 0
+        self.element_count = _["result"][0]["value"]
 
-        self.total_features = query_results_totals["result"][0]["value"]
-        if self.total_features != 0 and self.total_features is not None:
-            if self.edited_features > self.total_features:
-                # It can happen that features are counted which has been deleted since.
-                self.share_edited_features = 100
-            else:
-                self.share_edited_features = round(
-                    (self.edited_features / self.total_features) * 100
-                )
         return True
 
     def calculate(self) -> bool:
         logging.info(f"Calculation for indicator: {self.metadata.name}")
 
-        description = Template(self.metadata.result_description).substitute(
-            share=self.share_edited_features, layer_name=self.layer.name
-        )
-        if self.total_features == 0:
+        # It can be that features are counted, but have been deleted since.
+        if self.element_count == 0:
             self.result.label = "undefined"
             self.result.value = None
-            self.result.description = (
-                "Since the OHSOME query returned a count of 0 for this feature "
-                "a quality estimation can not be made for this filter"
-            )
+            self.result.description = "In the AOI no features are present."
             return False
-        elif self.share_edited_features >= self.threshold_yellow:
+
+        if self.contributions_latest_count > self.element_count:
+            self.ratio = 100
+        else:
+            self.ratio = round(
+                (self.contributions_latest_count / self.element_count) * 100
+            )
+
+        description = Template(self.metadata.result_description).substitute(
+            share=self.contributions_latest_count, layer_name=self.layer.name
+        )
+
+        if self.ratio >= self.threshold_yellow:
             self.result.label = "green"
             self.result.value = 1.0
             self.result.description = (
                 description + self.metadata.label_description["green"]
             )
-        elif self.share_edited_features >= self.threshold_red:
+        elif self.ratio >= self.threshold_red:
             self.result.label = "yellow"
             self.result.value = 0.5
             self.result.description = (
                 description + self.metadata.label_description["yellow"]
             )
-        else:
+        elif self.ratio < self.threshold_red:
             self.result.label = "red"
             self.result.value = 0.0
             self.result.description = (
                 description + self.metadata.label_description["red"]
             )
+
         return True
 
     def create_figure(self) -> bool:
@@ -136,7 +134,7 @@ class LastEdit(BaseIndicator):
 
         # Plot inner Pie (Indicator Value)
         radius = 1 - size
-        sizes = (100 - self.share_edited_features, self.share_edited_features)
+        sizes = (100 - self.ratio, self.ratio)
         colors = ("white", "black")
         ax.pie(
             sizes,
@@ -147,7 +145,7 @@ class LastEdit(BaseIndicator):
         )
 
         black_patch = mpatches.Patch(
-            color="black", label=f"{self.layer.name}: {self.share_edited_features} %"
+            color="black", label=f"{self.layer.name}: {self.ratio} %"
         )
         handles.append(black_patch)
 
