316cf0c538d4240bdff15821c5d825d9b6ad14b4
Matthias Schaub <matthias.schaub@heigit.org>
2021-07-12 16:09:43 +0200

    Checkout test_api.py from geo-interface-fix

--- workers/tests/unittests/test_api.py
+++ workers/tests/unittests/test_api.py
@@ -1,12 +1,13 @@
+import asyncio
 import os
 import unittest
 
 from geojson import Feature
 
-from ohsome_quality_analyst.utils.helper import loads_geojson
+from ohsome_quality_analyst.api import load_bpolys
 
 
-class TestUtilsHelper(unittest.TestCase):
+class TestApiUnit(unittest.TestCase):
     def test_load_bpolys_invalid(self):
         infile = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -17,34 +18,31 @@ class TestUtilsHelper(unittest.TestCase):
             bpolys = f.read()
 
         with self.assertRaises(ValueError):
-            for feature in loads_geojson(bpolys):
-                self.assertTrue(feature.is_valid)
-                self.assertIsInstance(feature, Feature)
+            asyncio.run(load_bpolys(bpolys))
 
-    # TODO
-    # def test_load_bpolys_size(self):
-    #     # No error should be raised
-    #     infile = os.path.join(
-    #         os.path.dirname(os.path.abspath(__file__)),
-    #         "fixtures",
-    #         "heidelberg-altstadt-geometry.geojson",
-    #     )
-    #     with open(infile, "r") as file:
-    #         bpolys = file.read()
-    #     bpolys = asyncio.run(load_bpolys(bpolys))
-    #     self.assertTrue(bpolys.is_valid)
-    #     self.assertIsInstance(bpolys, Feature)
+    def test_load_bpolys_size(self):
+        # No error should be raised
+        infile = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "fixtures",
+            "heidelberg-altstadt-geometry.geojson",
+        )
+        with open(infile, "r") as file:
+            bpolys = file.read()
+        bpolys = asyncio.run(load_bpolys(bpolys))
+        self.assertTrue(bpolys.is_valid)
+        self.assertIsInstance(bpolys, Feature)
 
-    #     # Error should be raised
-    #     infile = os.path.join(
-    #         os.path.dirname(os.path.abspath(__file__)),
-    #         "fixtures",
-    #         "europe.geojson",
-    #     )
-    #     with open(infile, "r") as f:
-    #         bpolys = f.read()
-    #     with self.assertRaises(ValueError):
-    #         asyncio.run(load_bpolys(bpolys))
+        # Error should be raised
+        infile = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "fixtures",
+            "europe.geojson",
+        )
+        with open(infile, "r") as f:
+            bpolys = f.read()
+        with self.assertRaises(ValueError):
+            asyncio.run(load_bpolys(bpolys))
 
     def test_load_bpolys_featurecollection_one_feature(self):
         infile = os.path.join(
@@ -54,9 +52,9 @@ class TestUtilsHelper(unittest.TestCase):
         )
         with open(infile, "r") as f:
             bpolys = f.read()
-        for feature in loads_geojson(bpolys):
-            self.assertTrue(feature.is_valid)
-            self.assertIsInstance(feature, Feature)
+        bpolys = asyncio.run(load_bpolys(bpolys))
+        self.assertTrue(bpolys.is_valid)
+        self.assertIsInstance(bpolys, Feature)
 
     def test_load_bpolys_featurecollection_multiple_features(self):
         infile = os.path.join(
@@ -66,9 +64,8 @@ class TestUtilsHelper(unittest.TestCase):
         )
         with open(infile, "r") as f:
             bpolys = f.read()
-        for feature in loads_geojson(bpolys):
-            self.assertTrue(feature.is_valid)
-            self.assertIsInstance(feature, Feature)
+        with self.assertRaises(ValueError):
+            asyncio.run(load_bpolys(bpolys))
 
     def test_load_bpolys_feature(self):
         infile = os.path.join(
@@ -78,9 +75,9 @@ class TestUtilsHelper(unittest.TestCase):
         )
         with open(infile, "r") as f:
             bpolys = f.read()
-        for feature in loads_geojson(bpolys):
-            self.assertTrue(feature.is_valid)
-            self.assertIsInstance(feature, Feature)
+        bpolys = asyncio.run(load_bpolys(bpolys))
+        self.assertTrue(bpolys.is_valid)
+        self.assertIsInstance(bpolys, Feature)
 
     def test_load_bpolys_geometry(self):
         infile = os.path.join(
@@ -90,6 +87,6 @@ class TestUtilsHelper(unittest.TestCase):
         )
         with open(infile, "r") as f:
             bpolys = f.read()
-        for feature in loads_geojson(bpolys):
-            self.assertTrue(feature.is_valid)
-            self.assertIsInstance(feature, Feature)
+        bpolys = asyncio.run(load_bpolys(bpolys))
+        self.assertTrue(bpolys.is_valid)
+        self.assertIsInstance(bpolys, Feature)
