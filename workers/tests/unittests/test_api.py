b1b564436301a2a6b0186d065d2e98749f2e74c3
Johannes Visintini <johannes.visintini@heigit.org>
2021-08-09 14:52:17 +0200

    replace http:// with https:// in mocked tests

--- workers/tests/unittests/test_api.py
+++ workers/tests/unittests/test_api.py
@@ -17,8 +17,8 @@ class TestApi(unittest.TestCase):
                 "text": "© OpenStreetMap contributors",
                 "url": "https://ohsome.org/copyrights",
             },
-            "requestUrl": "http://www.example.org/",
+            "requestUrl": "https://www.example.org/",
         }
         self.assertEqual(
-            response_template, empty_api_response("http://www.example.org/")
+            response_template, empty_api_response("https://www.example.org/")
         )
