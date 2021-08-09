6b0b7ed0f4b81b72cf3308417500a3d798ffbcf9
Martin Raifer <martin@raifer.tech>
2021-08-09 18:47:34 +0200

    drop unused code, and dependency to jQuery completely

--- website/website/assets/js/map-en.js
+++ website/website/assets/js/map-en.js
@@ -475,21 +475,6 @@ function buildMap(...charts){
 	};
 
 
-
-	// Text showing in info box before and while selecting a layer
-	$('.leaflet-control-layers-selector').click(function(){
-
-		var inside = document.getElementsByClassName("info").item(0);
-		// set timeslider to first year (2010)
-
-
-		if(map.hasLayer(world)) {
-			inside.innerHTML = '<p>Move the mouse over the map</p>';
-		}			;
-
-
-	});
-
 	// method that we will use to update the info box based on feature properties passed
 	info.updateInfo = function (id) {
 		// depending on selected layer, show corresponding information
