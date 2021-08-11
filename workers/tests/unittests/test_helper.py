GIScience/ohsome-quality-api

commit 8f6677d8a8ecd9a6cc4c60e5a985c30f9b5736ba

Change API response schema to be a valid GeoJSON  (#100)
* Change API response to return a GeoJSON

* Recreate VCR cassettes

* Add changes to CHANGELOG

Reformating and rewording of the CHANGELOG

* Minor changes to incorporate review feedback

Minor changes to logging message formatting

Add missing type hints

Minor changes to string concatenation syntax

Renamed variable to avoid overwriting existing var

* Recreate VCR cassettes

* urlencode parameters during tests

* recreate api_report_geojson_io VCR cassette

* Fix bug due wrong function parameters after rebase

with main.

* Add request url parameter to response_template

function. Rename url to request_url.

* Add tests for `empty_api_response` function

Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>

workers/tests/integrationtests/api_response_schema.py
@@ -1,0 +1,66 @@
+from typing import Optional
+
+from schema import Optional as Opt
+from schema import Or, Schema
+
+
+def generate_properties_template(iterator: Optional[str] = None) -> dict:
+    properties = {
+        "metadata.name": str,
+        "metadata.description": str,
+        "layer.name": str,
+        "layer.description": str,
+        "result.timestamp_oqt": str,
+        "result.timestamp_osm": Or(str, None),
+        "result.value": Or(float, None),
+        "result.label": str,
+        "result.description": str,
+        "result.svg": str,
+    }
+
+    if iterator is None:
+        return properties.copy()
+    else:
+        return {str(iterator) + "." + k: v for k, v in properties.items()}
+
+
+def get_report_properties_template() -> dict:
+    return {
+        "metadata.name": str,
+        "metadata.description": str,
+        "result.value": float,
+        "result.label": str,
+        "result.description": str,
+    }
+
+
+def get_response_schema() -> Schema:
+    return Schema(
+        {
+            "apiVersion": str,
+            "attribution": {
+                "text": str,
+                "url": str,
+            },
+            "requestUrl": str,
+        },
+        ignore_extra_keys=True,
+    )
+
+
+def get_feature_schema(iterator: Optional[str] = None):
+    return Schema(
+        {
+            "type": "Feature",
+            "geometry": dict,
+            Opt("id"): Or(str, int),
+            "properties": generate_properties_template(iterator),
+        },
+        ignore_extra_keys=True,
+    )
+
+
+def get_featurecollection_schema():
+    return Schema(
+        {"type": "FeatureCollection", "features": list}, ignore_extra_keys=True
+    )

workers/tests/unittests/test_helper.py
@@ -11,7 +11,20 @@ from ohsome_quality_analyst.utils.definitions import load_metadata
 from ohsome_quality_analyst.utils.helper import loads_geojson, name_to_class
 
 
+def get_fixture(name):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fixtures", name)
+    with open(path, "r") as f:
+        return f.read()
+
+
 class TestHelper(unittest.TestCase):
+    def run_tests(self, raw: str, number_of_features: int = 1) -> None:
+        i = 0
+        for feature in loads_geojson(raw):
+            i += 1
+            self.assertIsInstance(feature, Feature)
+        self.assertEqual(i, number_of_features)
+
     def test_name_to_class(self):
         self.assertIs(
             name_to_class(class_type="indicator", name="GhsPopComparisonBuildings"),
@@ -29,79 +42,30 @@ class TestHelper(unittest.TestCase):
                 name_to_class(class_type="indicator", name=indicator_name)
             )
 
-    def test_loads_geojson(self):
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "featurecollection.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
-        i = 0
-        for feature in loads_geojson(raw):
-            i += 1
-            self.assertIsInstance(feature, Feature)
-        self.assertEqual(i, 2)
+    def test_loads_geojson_geometry(self):
+        raw = get_fixture("heidelberg-altstadt-geometry.geojson")
+        self.run_tests(raw)
 
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "heidelberg-altstadt-featurecollection.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
-        i = 0
-        for feature in loads_geojson(raw):
-            i += 1
-            self.assertIsInstance(feature, Feature)
-        self.assertEqual(i, 1)
+    def test_loads_geojson_feature(self):
+        raw = get_fixture("heidelberg-altstadt-feature.geojson")
+        self.run_tests(raw)
 
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "heidelberg-altstadt-feature.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
-        i = 0
-        for feature in loads_geojson(raw):
-            i += 1
-            self.assertIsInstance(feature, Feature)
-        self.assertEqual(i, 1)
+    def test_loads_geojson_featurecollection(self):
+        raw = get_fixture("featurecollection.geojson")
+        self.run_tests(raw, number_of_features=2)
 
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "heidelberg-altstadt-geometry.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
-        i = 0
-        for feature in loads_geojson(raw):
-            i += 1
-            self.assertIsInstance(feature, Feature)
-        self.assertEqual(i, 1)
+    def test_loads_geojson_featurecollection_single_feature(self):
+        raw = get_fixture("heidelberg-altstadt-featurecollection.geojson")
+        self.run_tests(raw)
 
     def test_loads_geojson_invalid_geojson(self):
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "ohsome-response-200-invalid.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
+        raw = get_fixture("ohsome-response-200-invalid.geojson")
         with self.assertRaises(ValueError):
             for _ in loads_geojson(raw):
                 pass
 
     def test_loads_geojson_invalid_geometry_type(self):
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "fixtures",
-            "line-string.geojson",
-        )
-        with open(infile, "r") as file:
-            raw = file.read()
+        raw = get_fixture("line-string.geojson")
         with self.assertRaises(ValueError):
             for _ in loads_geojson(raw):
                 pass

workers/ohsome_quality_analyst/api.py
@@ -1,17 +1,14 @@
 import logging
 from typing import Optional
 
-import geojson
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from geojson import Feature, FeatureCollection, MultiPolygon, Polygon
 from pydantic import BaseModel
 
 from ohsome_quality_analyst import __version__ as oqt_version
 from ohsome_quality_analyst import oqt
 from ohsome_quality_analyst.geodatabase import client as db_client
-from ohsome_quality_analyst.utils.definitions import GEOM_SIZE_LIMIT, configure_logging
-from ohsome_quality_analyst.utils.helper import name_to_lower_camel
+from ohsome_quality_analyst.utils.definitions import configure_logging
 
 configure_logging()
 logging.info("Logging enabled")
@@ -42,48 +39,15 @@ class ReportParameters(BaseModel):
     fidField: Optional[str] = None
 
 
-def empty_api_response() -> dict:
+def empty_api_response(request_url: str) -> dict:
     return {
-        "attribution": {
-            "url": "https://ohsome.org/copyrights",
-            "text": "© OpenStreetMap contributors",
-        },
         "apiVersion": oqt_version,
+        "attribution": {
+            "text": "© OpenStreetMap contributors",
+            "url": "https://ohsome.org/copyrights",
+        },
+        "requestUrl": request_url,
     }
-
-
-async def load_bpolys(bpolys: str) -> Feature:
-    """Load as GeoJSON object, validate and check size of bpolys"""
-    # TODO: Return API response with error message
-
-    async def check_geom_size(geom):
-        if await db_client.get_area_of_bpolys(geom) > GEOM_SIZE_LIMIT:
-            raise ValueError(
-                "Input GeoJSON Object is too big. "
-                + "The area should be less than {0} sqkm.".format(GEOM_SIZE_LIMIT)
-            )
-
-    bpolys = geojson.loads(bpolys)
-
-    if bpolys.is_valid is False:
-        raise ValueError("Input geometry is not valid")
-    elif isinstance(bpolys, FeatureCollection):
-        if len(bpolys.features) == 1:
-            feature = bpolys.features[0]
-            await check_geom_size(feature.geometry)
-            return feature
-        else:
-            raise ValueError("Only one Feature is supported in a FeatureCollection")
-    elif isinstance(bpolys, Feature):
-        await check_geom_size(bpolys.geometry)
-        return bpolys
-    elif isinstance(bpolys, (Polygon, MultiPolygon)):
-        await check_geom_size(bpolys)
-        return Feature(geometry=bpolys)
-    else:
-        raise ValueError(
-            "Input GeoJSON Objects have to be of type Feature, Polygon or MultiPolygon"
-        )
 
 
 @app.get("/indicator/{name}")
@@ -96,9 +60,8 @@ async def get_indicator(
     featureId: Optional[str] = None,
     fidField: Optional[str] = None,
 ):
-    url = request.url._url
     return await _fetch_indicator(
-        name, layerName, url, bpolys, dataset, featureId, fidField
+        name, layerName, request.url._url, bpolys, dataset, featureId, fidField
     )
 
 
@@ -110,37 +73,31 @@ async def post_indicator(name: str, request: Request, item: IndicatorParameters)
     dataset = item.dict().get("dataset", None)
     feature_id = item.dict().get("featureId", None)
     fid_field = item.dict().get("fidField", None)
-    url = request.url._url
     return await _fetch_indicator(
-        name, layer_name, url, bpolys, dataset, feature_id, fid_field
+        name, layer_name, request.url._url, bpolys, dataset, feature_id, fid_field
     )
 
 
 async def _fetch_indicator(
     name: str,
     layer_name: str,
-    url: str,
+    request_url: str,
     bpolys: Optional[str] = None,
     dataset: Optional[str] = None,
     feature_id: Optional[str] = None,
     fid_field: Optional[str] = None,
-):
-    if bpolys is not None:
-        feature = await load_bpolys(bpolys)
-    else:
-        feature = None
-
-    indicator = await oqt.create_indicator(
-        name, layer_name, feature, dataset, feature_id, fid_field
+) -> dict:
+    geojson_object = await oqt.create_indicator_as_geojson(
+        name,
+        layer_name,
+        bpolys,
+        dataset,
+        feature_id,
+        fid_field,
+        size_restriction=True,
     )
-
-    response = empty_api_response()
-    response["metadata"] = vars(indicator.metadata)
-    response["metadata"]["requestUrl"] = url
-    response["metadata"].pop("result_description", None)
-    response["metadata"].pop("label_description", None)
-    response["layer"] = vars(indicator.layer)
-    response["result"] = vars(indicator.result)
+    response = empty_api_response(request_url)
+    response.update(geojson_object)
     return response
 
 
@@ -153,8 +110,9 @@ async def get_report(
     featureId: Optional[str] = None,
     fidField: Optional[str] = None,
 ):
-    url = request.url._url
-    return await _fetch_report(name, url, bpolys, dataset, featureId, fidField)
+    return await _fetch_report(
+        name, request.url._url, bpolys, dataset, featureId, fidField
+    )
 
 
 @app.post("/report/{name}")
@@ -163,51 +121,29 @@ async def post_report(name: str, request: Request, item: ReportParameters):
     dataset = item.dict().get("dataset", None)
     feature_id = item.dict().get("featureId", None)
     fid_field = item.dict().get("fidField", None)
-    url = request.url._url
-    return await _fetch_report(name, url, bpolys, dataset, feature_id, fid_field)
+    return await _fetch_report(
+        name, request.url._url, bpolys, dataset, feature_id, fid_field
+    )
 
 
 async def _fetch_report(
     name: str,
-    url: str,
+    request_url: str,
     bpolys: Optional[str] = None,
     dataset: Optional[str] = None,
     feature_id: Optional[str] = None,
     fid_field: Optional[str] = None,
 ):
-    if bpolys is not None:
-        feature = await load_bpolys(bpolys)
-    else:
-        feature = None
-
-    report = await oqt.create_report(
+    geojson_object = await oqt.create_report_as_geojson(
         name,
-        feature=feature,
+        bpolys=bpolys,
         dataset=dataset,
         feature_id=feature_id,
         fid_field=fid_field,
+        size_restriction=True,
     )
-
-    response = empty_api_response()
-    response["metadata"] = vars(report.metadata)
-    response["metadata"]["requestUrl"] = url
-    response["metadata"].pop("label_description", None)
-    response["result"] = vars(report.result)
-    response["result"]["label"] = report.result.label
-    response["indicators"] = {}
-    for indicator in report.indicators:
-        metadata = vars(indicator.metadata)
-        metadata.pop("result_description", None)
-        metadata.pop("label_description", None)
-        layer = vars(indicator.layer)
-        result = vars(indicator.result)
-        indicator_name = name_to_lower_camel(metadata["name"])
-        layer_name = name_to_lower_camel(layer["name"])
-        response["indicators"][indicator_name + layer_name] = {
-            "metadata": metadata,
-            "layer": layer,
-            "result": result,
-        }
+    response = empty_api_response(request_url)
+    response.update(geojson_object)
     return response
 
 

workers/ohsome_quality_analyst/base/indicator.py
@@ -92,7 +92,6 @@ class BaseIndicator(metaclass=ABCMeta):
         The geometry (and properties) of the input GeoJSON object is preserved.
         """
         result = vars(self.result).copy()
-        result.pop("svg")
         # Prefix all keys of the dictionary
         properties = {
             "metadata.name": self.metadata.name,

workers/ohsome_quality_analyst/cli.py
@@ -4,7 +4,6 @@ import logging
 import click
 import geojson
 import yaml
-from geojson import FeatureCollection
 
 from ohsome_quality_analyst import cli_opts, oqt
 from ohsome_quality_analyst.geodatabase import client as db_client
@@ -16,7 +15,6 @@ from ohsome_quality_analyst.utils.definitions import (
 )
 from ohsome_quality_analyst.utils.helper import (
     datetime_to_isostring_timestamp,
-    loads_geojson,
     write_geojson,
 )
 
@@ -118,36 +116,19 @@ def create_indicator(
     if infile is not None:
         with open(infile, "r") as file:
             bpolys = file.read()
-        features = []
-        for i, feature in enumerate(loads_geojson(bpolys)):
-            logging.info("Input feature index:\t" + str(i))
-            indicator = asyncio.run(
-                oqt.create_indicator(
-                    indicator_name,
-                    layer_name,
-                    feature=feature,
-                    feature_id=feature_id,
-                    dataset=dataset_name,
-                    fid_field=fid_field,
-                    force=force,
-                )
-            )
-            features.append(indicator.as_feature())
-        geojson_object = FeatureCollection(features=features)
     else:
-        # When using a dataset and FID as input
-        indicator = asyncio.run(
-            oqt.create_indicator(
-                indicator_name,
-                layer_name,
-                feature=None,
-                feature_id=feature_id,
-                dataset=dataset_name,
-                fid_field=fid_field,
-                force=force,
-            )
+        bpolys = None
+    geojson_object = asyncio.run(
+        oqt.create_indicator_as_geojson(
+            indicator_name,
+            layer_name,
+            bpolys,
+            dataset_name,
+            feature_id,
+            fid_field,
+            force,
         )
-        geojson_object = indicator.as_feature()
+    )
     if outfile:
         write_geojson(outfile, geojson_object)
     click.echo(
@@ -188,34 +169,18 @@ def create_report(
     if infile is not None:
         with open(infile, "r") as file:
             bpolys = file.read()
-        features = []
-        for i, feature in enumerate(loads_geojson(bpolys)):
-            logging.info("Input feature index:\t" + str(i))
-            report = asyncio.run(
-                oqt.create_report(
-                    report_name,
-                    feature=feature,
-                    dataset=dataset_name,
-                    feature_id=feature_id,
-                    fid_field=fid_field,
-                    force=force,
-                )
-            )
-            features.append(report.as_feature())
-        geojson_object = FeatureCollection(features=features)
     else:
-        # When using a dataset and FID as input
-        report = asyncio.run(
-            oqt.create_report(
-                report_name,
-                feature=None,
-                dataset=dataset_name,
-                feature_id=feature_id,
-                fid_field=fid_field,
-                force=force,
-            )
+        bpolys = None
+    geojson_object = asyncio.run(
+        oqt.create_report_as_geojson(
+            report_name,
+            bpolys,
+            dataset_name,
+            feature_id,
+            fid_field,
+            force,
         )
-        geojson_object = report.as_feature()
+    )
     if outfile:
         write_geojson(outfile, geojson_object)
     click.echo(

commit 151c472f088feb48795b0f4031e0349a08b7a6bc

add report (non-indicator) properties to GeoJSON output
fixes bug introduced in #100

workers/ohsome_quality_analyst/base/report.py
@@ -9,6 +9,7 @@ from geojson import Feature
 
 from ohsome_quality_analyst.base.indicator import BaseIndicator
 from ohsome_quality_analyst.utils.definitions import get_metadata
+from ohsome_quality_analyst.utils.helper import flatten_dict
 
 
 @dataclass
@@ -64,10 +65,18 @@ class BaseReport(metaclass=ABCMeta):
         The properties of the Feature contains the attributes of all indicators.
         The geometry (and properties) of the input GeoJSON object is preserved.
         """
-        properties = {}
+        report_properties = {
+            "metadata": vars(self.metadata).copy(),
+            "result": vars(self.result).copy(),
+        }
+        report_properties["metadata"].pop("label_description", None)
+        # TODO: do we need a request url?
+        properties = flatten_dict(report_properties, prefix="report")
         for i, indicator in enumerate(self.indicators):
             p = indicator.as_feature()["properties"]
-            properties.update({str(i) + "." + str(key): val for key, val in p.items()})
+            properties.update(
+                {"indicators." + str(i) + "." + str(key): val for key, val in p.items()}
+            )
         return Feature(geometry=self.feature.geometry, properties=properties)
 
     @abstractmethod

workers/ohsome_quality_analyst/utils/helper.py
@@ -108,3 +108,18 @@ def loads_geojson(bpolys: str) -> Generator[Feature, None, None]:
         raise ValueError(
             "Input GeoJSON Objects have to be of type Feature, Polygon or MultiPolygon"
         )
+
+
+def flatten_dict(input: dict, *, separator: str = ".", prefix: str = "") -> dict:
+    """Returns the given dict as flattened one-level dict."""
+    if isinstance(input, dict):
+        output = {}
+        if prefix != "":
+            prefix += separator
+        for key, value in input.items():
+            output.update(
+                flatten_dict(input[key], separator=separator, prefix=prefix + key)
+            )
+        return output
+    else:
+        return {prefix: input}

workers/tests/integrationtests/api_response_schema.py
@@ -21,7 +21,9 @@ def generate_properties_template(iterator: Optional[str] = None) -> dict:
     if iterator is None:
         return properties.copy()
     else:
-        return {str(iterator) + "." + k: v for k, v in properties.items()}
+        return {
+            "indicators." + str(iterator) + "." + k: v for k, v in properties.items()
+        }
 
 
 def get_report_properties_template() -> dict:

workers/tests/integrationtests/test_base_report.py
@@ -25,10 +25,10 @@ class TestBaseReport(unittest.TestCase):
         self.assertTrue(feature.is_valid)
 
         for i in (
-            "0.data.pop_count",
-            "0.data.area",
-            "0.data.pop_count_per_sqkm",
-            "0.data.feature_count",
-            "0.data.feature_count_per_sqkm",
+            "indicators.0.data.pop_count",
+            "indicators.0.data.area",
+            "indicators.0.data.pop_count_per_sqkm",
+            "indicators.0.data.feature_count",
+            "indicators.0.data.feature_count_per_sqkm",
         ):
             self.assertIn(i, feature["properties"].keys())

workers/tests/unittests/test_helper.py
@@ -8,7 +8,11 @@ from ohsome_quality_analyst.indicators.ghs_pop_comparison_buildings.indicator im
 )
 from ohsome_quality_analyst.reports.simple_report.report import SimpleReport
 from ohsome_quality_analyst.utils.definitions import load_metadata
-from ohsome_quality_analyst.utils.helper import loads_geojson, name_to_class
+from ohsome_quality_analyst.utils.helper import (
+    flatten_dict,
+    loads_geojson,
+    name_to_class,
+)
 
 
 def get_fixture(name):
@@ -69,6 +73,11 @@ class TestHelper(unittest.TestCase):
         with self.assertRaises(ValueError):
             for _ in loads_geojson(raw):
                 pass
+
+    def test_flatten_dict(self):
+        deep = {"foo": {"bar": "baz", "lang": {"нет": "tak"}}, "something": 5}
+        flat = {"foo.bar": "baz", "foo.lang.нет": "tak", "something": 5}
+        self.assertDictEqual(flatten_dict(deep), flat)
 
 
 if __name__ == "__main__":