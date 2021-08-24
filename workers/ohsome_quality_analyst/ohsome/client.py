c54df9f66dcf08772de8a2ae84d4fa4ba9be55b4
Johannes Visintini <johannes.visintini@heigit.org>
2021-08-24 18:27:29 +0200

    increase general timeouts in ohsome client to 300 seconds

--- workers/ohsome_quality_analyst/ohsome/client.py
+++ workers/ohsome_quality_analyst/ohsome/client.py
@@ -37,7 +37,7 @@ async def query(
 async def query_ohsome_api(url: str, data: dict, headers: dict = {}) -> dict:
     # custom timeout as ohsome API can take a long time to send an answer
     # (up to 10 minutes)
-    timeout = httpx.Timeout(5, read=660)
+    timeout = httpx.Timeout(300, read=660)
     headers["user-agent"] = USER_AGENT
     async with httpx.AsyncClient(timeout=timeout) as client:
         resp = await client.post(url, data=data, headers=headers)
