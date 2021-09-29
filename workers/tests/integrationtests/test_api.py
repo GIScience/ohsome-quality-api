07b6121109db22481f8a8b061d27311d2b5967d9
Matthias Schaub <83658582+matthiasschaub@users.noreply.github.com>
2021-09-29 15:07:17 +0200

    Add and improve tests for API (#168)
    * Add and improve tests for API
    
    Add test cases covering the content of GeoJSON properties.
    
    Improve API response schemata by using less logic to create the
    schemata.
    
    * Improve docstring based on review
    
    * Define indicator properties only once
    
    * Removed unused function argument
    
    * Make code more elegant by avoiding duplications

--- workers/tests/integrationtests/test_api.py
+++ workers/tests/integrationtests/test_api.py
@@ -11,47 +11,68 @@ from fastapi.testclient import TestClient
 
 from ohsome_quality_analyst.api.api import app
 
+from .api_response_schema import get_general_schema, get_result_schema
+
 
 class TestApi(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
+        self.general_schema = get_general_schema()
+        self.result_schema = get_result_schema()
 
     def test_get_available_regions(self):
         url = "/regions"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        _geojson = json.dumps(json.loads(response.content))
-        self.assertTrue(geojson.loads(_geojson).is_valid)
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        result = geojson.loads(json.dumps(response_content))
+        self.assertTrue(result.is_valid)
 
     def test_get_list_indicators(self):
         url = "/indicatorNames"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(geojson.loads(response.content), dict)
+
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        self.assertTrue(self.result_schema.is_valid(response_content))
 
     def test_get_list_layers(self):
         url = "/layerNames"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(geojson.loads(response.content), dict)
+
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        self.assertTrue(self.result_schema.is_valid(response_content))
 
     def test_get_list_datasets(self):
         url = "/datasetNames"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(geojson.loads(response.content), dict)
+
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        self.assertTrue(self.result_schema.is_valid(response_content))
 
     def test_get_list_fid_field(self):
         url = "/FidFields"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(geojson.loads(response.content), dict)
+
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        self.assertTrue(self.result_schema.is_valid(response_content))
 
     def test_get_list_reports(self):
         url = "/reportNames"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(geojson.loads(response.content), dict)
+
+        response_content = json.loads(response.content)
+        self.assertTrue(self.general_schema.is_valid(response_content))
+        self.assertTrue(self.result_schema.is_valid(response_content))
 
 
 if __name__ == "__main__":
