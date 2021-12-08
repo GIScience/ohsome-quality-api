8e1794a2c5d31a735e3778006aad7877eac920c3
Matthias Schaub <matthias.schaub@heigit.org>
2021-12-08 12:30:04 +0100

    Extend script to run all statistical models of the
    Mapping Saturation indicator for input data covering all OQT regions.

--- workers/scripts/run_mapping_saturation_models.py
+++ workers/scripts/run_mapping_saturation_models.py
@@ -1,16 +1,21 @@
 """Run all statistical models of the Mapping Saturation indicator and plot them."""
 
+import asyncio
+import pickle
+from os.path import exists
+
 import matplotlib.pyplot as plt
 import numpy as np
+from dacite import from_dict
 
+import ohsome_quality_analyst.geodatabase.client as db_client
+import ohsome_quality_analyst.ohsome.client as ohsome_client
+from ohsome_quality_analyst.base.indicator import LayerDefinition
 from ohsome_quality_analyst.indicators.mapping_saturation.fit import run_all_models
-from tests.unittests.mapping_saturation.fixtures import VALUES
+from ohsome_quality_analyst.utils.definitions import get_layer_definition
 
 
-def main():
-    xdata = np.asarray(range(len(VALUES)))
-    ydata = VALUES
-    fitted_models = run_all_models(xdata, ydata)
+def plot(xdata, ydata, fitted_models):
     plt.figure()
     prev_ax = None
     for i, model in enumerate(fitted_models):
@@ -33,5 +38,54 @@ def main():
     plt.show()
 
 
+async def query_ohsome_api(features, layers) -> list:
+    time_range = "2008-01-01//P1M"
+    list_of_values = []
+    for feature in features:
+        for layer in layers:
+            query_results = await ohsome_client.query(
+                layer=layer, bpolys=feature.geometry, time=time_range
+            )
+            list_of_values.append([item["value"] for item in query_results["result"]])
+    return list_of_values
+
+
+def get_layers(layer_names) -> list:
+    layers = []
+    for layer_name in layer_names:
+        layers.append(
+            from_dict(data_class=LayerDefinition, data=get_layer_definition(layer_name))
+        )
+    return layers
+
+
+async def get_features() -> list:
+    fids = await db_client.get_feature_ids("regions")
+    features = []
+    for fid in fids:
+        features.append(await db_client.get_feature_from_db("regions", fid))
+    return features
+
+
 if __name__ == "__main__":
-    main()
+    file_path = "data.pkl"
+    if exists(file_path):
+        with open(file_path, "rb") as f:
+            list_of_values = pickle.load(f)
+    else:
+        features = asyncio.run(get_features())
+        layers = get_layers(("building_count", "major_roads_length"))
+        list_of_values = asyncio.run(query_ohsome_api(features, layers))
+        with open(file_path, "wb") as f:
+            pickle.dump(list_of_values, f)
+    total_num_of_models = 0
+    for values in list_of_values:
+        xdata = np.asarray(range(len(values)))
+        ydata = np.asarray(values)
+        fitted_models = run_all_models(xdata, ydata)
+        plot(xdata, ydata, fitted_models)
+        total_num_of_models += len(fitted_models)
+    print("Expected number of models: " + str(len(list_of_values) * 6))
+    print("Total number of models: " + str(total_num_of_models))
+    # Expected number of models: 192
+    # Total number of models: 143
