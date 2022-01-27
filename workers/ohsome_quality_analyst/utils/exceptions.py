bf30538adba599ec090f6885a6fd7b6f7297dda0
Matthias Schaub <matthias.schaub@heigit.org>
2022-01-27 08:19:26 +0100

    Avoid circular import by not importing global
    constant from definitions.py in exceptions.py

--- workers/ohsome_quality_analyst/utils/exceptions.py
+++ workers/ohsome_quality_analyst/utils/exceptions.py
@@ -1,10 +1,8 @@
-"""Custom exception classes"""
-
-from ohsome_quality_analyst.utils.definitions import GEOM_SIZE_LIMIT
+"""Custom exception classes."""
 
 
 class OhsomeApiError(Exception):
-    """Request to ohsome API failed"""
+    """Request to ohsome API failed."""
 
     def __init__(self, message):
         self.name = "OhsomeApiError"
@@ -14,9 +12,9 @@ class OhsomeApiError(Exception):
 class SizeRestrictionError(ValueError):
     """Exception raised if size of input GeoJSON Geometry is too big."""
 
-    def __init__(self):
+    def __init__(self, geom_size_limit):
         self.name = "SizeRestrictionError"
         self.message = (
             "Input GeoJSON Geometry is too big. "
-            "The area should be less than {0} km².".format(GEOM_SIZE_LIMIT)
+            + "The area should be less than {0} km².".format(geom_size_limit)
         )
