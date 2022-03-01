d85787db1b44770a0cca86b181f8d10d781a7a0a
Matthias Schaub <matthias.schaub@heigit.org>
2022-03-01 14:16:30 +0100

    Remove verbose constructors; Fall back to default.

--- workers/ohsome_quality_analyst/reports/sketchmap_fitness/report.py
+++ workers/ohsome_quality_analyst/reports/sketchmap_fitness/report.py
@@ -1,12 +1,7 @@
-from geojson import Feature
-
 from ohsome_quality_analyst.base.report import BaseReport, IndicatorLayer
 
 
 class SketchmapFitness(BaseReport):
-    def __init__(self, feature: Feature = None) -> None:
-        super().__init__(feature=feature)
-
     def set_indicator_layer(self):
         self.indicator_layer = (
             IndicatorLayer("MappingSaturation", "major_roads_length"),
