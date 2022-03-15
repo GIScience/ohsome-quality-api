0bd4252c7b1f3678f94b918ebc2c4aa68c3b8dda
Matthias Schaub <matthias.schaub@heigit.org>
2022-03-15 07:19:39 +0100

    Add index to SHDI database table

--- database/init_db.production/SHDI.sh
+++ database/init_db.production/SHDI.sh
@@ -4,3 +4,4 @@
 wget -U oqt https://globaldatalab.org/assets/2020/03/GDL%20Shapefiles%20V4.zip
 unzip "GDL Shapefiles V4.zip"
 shp2pgsql -d -s 4326 GDL\ Shapefiles\ V4.shp shdi | psql
+psql -f SHDI.sql
