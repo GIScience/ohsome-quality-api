c59918e5fa105abe14136c4b115244ebc2e7ace0
Matthias Schaub <matthias.schaub@heigit.org>
2022-06-01 10:33:38 +0200

    download hexcells and shdi SQL files for dev db

--- database/init_db.development/schema.dev.sh
+++ database/init_db.development/schema.dev.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# SQL files will be executed on first start of the database container
 set -e
-
 if [[ $OQT_TEST_DB = False ]]; then
     wget https://downloads.ohsome.org/OQT/schema.dev.sql.gz
     gunzip schema.dev.sql.gz
+    wget https://downloads.ohsome.org/OQT/hexcells.sql.gz
+    gunzip hexcells.sql.gz
+    wget https://downloads.ohsome.org/OQT/shdi.sql.gz
+    gunzip shdi.sql.gz
 fi
