a6026409b4eabade39e419fe276cfca1f053439d
Gigaszi <levi@szamek.de>
2022-06-07 12:49:10 +0200

    Add building_report and add ratio_filter to building_count layer

---
+++ workers/tests/unittests/test_report_building_report.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import Mock
+
+from ohsome_quality_analyst.reports.jrc_requirements.report import JrcRequirements
+
+from .utils import get_geojson_fixture
+
+
+class TestReportJrcRequirements(unittest.TestCase):
+    # TODO: Test case for indicator.result undefined
+    def test_combine_indicators_mean(self):
+
+        geometry = get_geojson_fixture("heidelberg-altstadt-geometry.geojson")
+        report = JrcRequirements(geometry)
+        report.set_indicator_layer()
+
+        # Mock indicator objects with a fixed result value
+        for _ in report.indicator_layer:
+            indicator = Mock()
+            indicator.result = Mock()
+            indicator.result.value = 0.5
+            indicator.result.html = "foo"
+            report.indicators.append(indicator)
+
+        report.combine_indicators()
+        report.create_html()
+
+        self.assertIsNotNone(report.result.label)
+        self.assertIsNotNone(report.result.description)
+        self.assertIsNotNone(report.result.html)
+        self.assertEqual(report.result.value, 0.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
