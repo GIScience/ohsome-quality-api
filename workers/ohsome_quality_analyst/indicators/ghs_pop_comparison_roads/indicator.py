ffa01b86103d72b48ba4d7d17f4f0f266633433a
Matthias Schaub <matthias.schaub@heigit.org>
2022-06-21 14:02:53 +0200

    Revert NoData check when retrieving raster stats
    This reverts commit db7bab27ea3c66242fb259b8d6156f9bb3e3b287.

--- workers/ohsome_quality_analyst/indicators/ghs_pop_comparison_roads/indicator.py
+++ workers/ohsome_quality_analyst/indicators/ghs_pop_comparison_roads/indicator.py
@@ -47,7 +47,7 @@ class GhsPopComparisonRoads(BaseIndicator):
 
     async def preprocess(self) -> None:
         raster = get_raster_dataset("GHS_POP_R2019A")
-        pop_count = get_zonal_stats(self.feature, raster, stats=["sum"])[0]["sum"]
+        pop_count = get_zonal_stats(self.feature, raster, stats="sum")[0]["sum"]
         area = await get_area_of_bpolys(self.feature.geometry)
         if pop_count is None:
             pop_count = 0
