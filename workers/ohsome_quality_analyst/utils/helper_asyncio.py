630837da2333723dae5f1ba2479e8c2b2e447938
Matthias Schaub <matthias.schaub@heigit.org>
2022-06-21 09:19:19 +0200

    make `gather_with_semaphore` a wrapper of `gather`
    function of the `asyncio` module

--- workers/ohsome_quality_analyst/utils/helper_asyncio.py
+++ workers/ohsome_quality_analyst/utils/helper_asyncio.py
@@ -3,8 +3,8 @@ import asyncio
 from typing import Coroutine
 
 
-async def gather_with_semaphore(tasks: list) -> Coroutine:
-    """Limit the number of tasks executed at a time."""
+async def gather_with_semaphore(tasks: list, *args, **kwargs) -> Coroutine:
+    """A wrapper around `gather` to limit the number of tasks executed at a time."""
     # Semaphore needs to initiated inside of the event loop
     semaphore = asyncio.Semaphore(4)
 
@@ -12,4 +12,4 @@ async def gather_with_semaphore(tasks: list) -> Coroutine:
         async with semaphore:
             return await task
 
-    return await asyncio.gather(*(sem_task(task) for task in tasks))
+    return await asyncio.gather(*(sem_task(task) for task in tasks), *args, **kwargs)
