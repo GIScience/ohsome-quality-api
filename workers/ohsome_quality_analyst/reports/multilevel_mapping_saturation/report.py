f2f7dfeeeafa8898273f75f416d85305482e6748
Gigaszi <levi@szamek.de>
2022-07-12 16:34:23 +0200

    fix metadata.yaml

--- workers/ohsome_quality_analyst/reports/multilevel_mapping_saturation/report.py
+++ workers/ohsome_quality_analyst/reports/multilevel_mapping_saturation/report.py
@@ -5,7 +5,7 @@ from ohsome_quality_analyst.utils.definitions import get_attribution
 class MultilevelMappingSaturation(BaseReport):
     def set_indicator_layer(self):
         self.indicator_layer = (
-            IndicatorLayer("MappingSaturation", "ideal_vgi_infrastructur"),
+            IndicatorLayer("MappingSaturation", "ideal_vgi_infrastructure"),
             IndicatorLayer("MappingSaturation", "poi"),
             IndicatorLayer("MappingSaturation", "lulc"),
             IndicatorLayer("MappingSaturation", "building_count"),
