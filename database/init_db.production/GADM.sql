c7479e754cefeca93e64eade4568d35bff1bb2b9
Matthias Schaub <matthias.schaub@heigit.org>
2022-07-14 10:41:40 +0200

    refactor(db): remove old production init scripts

--- database/init_db.production/GADM.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE level0 RENAME TO gadm_level_0;
-ALTER TABLE level1 RENAME TO gadm_level_1;
-ALTER TABLE level2 RENAME TO gadm_level_2;
-ALTER TABLE level3 RENAME TO gadm_level_3;
-ALTER TABLE level4 RENAME TO gadm_level_4;
-ALTER TABLE level5 RENAME TO gadm_level_5;
--- database/init_db.production/SHDI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Human Development Indices
-# https://globaldatalab.org/shdi/shapefiles/
-wget -U oqt https://globaldatalab.org/assets/2020/03/GDL%20Shapefiles%20V4.zip
-unzip "GDL Shapefiles V4.zip"
-shp2pgsql -d -s 4326 GDL\ Shapefiles\ V4.shp shdi | psql
-psql -f SHDI.sql
