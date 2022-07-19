6875a6e1711f87c971251e62946575fe92b9b6a6
Matthias Schaub <matthias.schaub@heigit.org>
2022-07-19 09:38:12 +0200

    test: rewrite using pytest instead of unittest

--- workers/tests/integrationtests/test_indicator_building_completeness.py
+++ workers/tests/integrationtests/test_indicator_building_completeness.py
@@ -2,11 +2,10 @@ import asyncio
 import os
 import unittest
 from datetime import datetime
-from unittest import mock
 
+import pytest
 from geojson import FeatureCollection
 
-from ohsome_quality_analyst.geodatabase import client as db_client
 from ohsome_quality_analyst.indicators.building_completeness.indicator import (
     BuildingCompleteness,
     get_hex_cells,
@@ -15,97 +14,94 @@ from ohsome_quality_analyst.indicators.building_completeness.indicator import (
 )
 from ohsome_quality_analyst.utils.exceptions import HexCellsNotFoundError
 
-from .utils import get_layer_fixture, oqt_vcr
+from .utils import get_fixture_dir, get_geojson_fixture, get_layer_fixture, oqt_vcr
 
 
-class TestIndicatorBuildingCompleteness(unittest.TestCase):
-    def setUp(self):
-        self.feature = asyncio.run(
-            db_client.get_feature_from_db(dataset="regions", feature_id="12")
-        )
-        self.layer = get_layer_fixture("building_area")
-        self.indicator = BuildingCompleteness(feature=self.feature, layer=self.layer)
+@pytest.fixture
+def mock_env_oqt_data_dir(monkeypatch):
+    directory = os.path.join(get_fixture_dir(), "rasters")
+    monkeypatch.setenv("OQT_DATA_DIR", directory)
 
-    @mock.patch("ohsome_quality_analyst.raster.client.get_config_value")
-    @oqt_vcr.use_cassette()
-    def test_indicator(self, mock_get_data_dir):
-        mock_get_data_dir.return_value = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "fixtures", "raster"
-        )
-        asyncio.run(self.indicator.preprocess())
-        # Data
-        self.assertIsInstance(self.indicator.building_area_osm, list)
-        self.assertEqual(len(self.indicator.building_area_osm), 9)
-        self.assertIsInstance(self.indicator.hex_cell_geohash, list)
-        self.assertEqual(len(self.indicator.hex_cell_geohash), 9)
-        # Covariates
-        self.assertIsInstance(self.indicator.covariates, dict)
-        self.assertEqual(len(self.indicator.covariates), 12)
-        for key in (
-            "urban_centre",
-            "dense_urban_cluster",
-            "semi_dense_urban_cluster",
-            "suburban_or_peri_urban",
-            "rural_cluster",
-            "low_density_rural",
-            "very_low_density_rural",
-            "water",
-        ):
-            self.assertEqual(len(self.indicator.covariates[key]), 9)
-            for i in self.indicator.covariates[key]:
-                self.assertIsNotNone(i)
-                self.assertGreaterEqual(i, 0)
-        # Calculate
-        self.indicator.calculate()
-        self.assertIsInstance(self.indicator.building_area_prediction, list)
-        self.assertGreaterEqual(len(self.indicator.building_area_prediction), 0)
-        self.assertIsInstance(self.indicator.result.timestamp_osm, datetime)
-        self.assertIsInstance(self.indicator.result.timestamp_oqt, datetime)
-        self.assertIsNotNone(self.indicator.result.label)
-        self.assertIsNotNone(self.indicator.result.value)
-        self.assertIsNotNone(self.indicator.result.description)
-        self.assertLessEqual(self.indicator.result.value, 1.0)
-        self.assertGreaterEqual(self.indicator.result.value, 0.0)
-        # Create Figure
-        self.indicator.create_figure()
-        self.assertIsNotNone(self.indicator.result.svg)
 
-    @mock.patch("ohsome_quality_analyst.raster.client.get_config_value")
-    def test_get_smod_class_share(self, mock_get_data_dir):
-        mock_get_data_dir.return_value = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "fixtures", "raster"
-        )
-        result = get_smod_class_share(FeatureCollection(features=[self.feature]))
-        self.assertDictEqual(
-            result,
-            {
-                "urban_centre": [0.05128205128205128],
-                "dense_urban_cluster": [0],
-                "semi_dense_urban_cluster": [0],
-                "suburban_or_peri_urban": [0.029914529914529916],
-                "rural_cluster": [0],
-                "low_density_rural": [0.017094017094017096],
-                "very_low_density_rural": [0.9017094017094017],
-                "water": [0],
-            },
-        )
+@pytest.fixture
+def feature():
+    return get_geojson_fixture("algeria-touggourt-feature.geojson")
 
-    def test_get_hex_cells(self):
-        result = asyncio.run(get_hex_cells(self.feature))
-        self.assertIsInstance(result, FeatureCollection)
-        self.assertIsNotNone(result.features)
 
-    def test_get_hex_cells_not_found(self):
-        feature = asyncio.run(
-            db_client.get_feature_from_db(dataset="regions", feature_id="3")
-        )
-        with self.assertRaises(HexCellsNotFoundError):
+@pytest.fixture
+def layer():
+    return get_layer_fixture("building_area")
+
+
+@oqt_vcr.use_cassette()
+def test_indicator(mock_env_oqt_data_dir, feature, layer):
+    indicator = BuildingCompleteness(feature=feature, layer=layer)
+
+    asyncio.run(indicator.preprocess())
+    # Data
+    assert isinstance(indicator.building_area_osm, list)
+    assert len(indicator.building_area_osm) == 9
+    assert isinstance(indicator.hex_cell_geohash, list)
+    assert len(indicator.hex_cell_geohash) == 9
+    # Covariates
+    assert isinstance(indicator.covariates, dict)
+    assert len(indicator.covariates) == 12
+    for key in (
+        "urban_centre",
+        "dense_urban_cluster",
+        "semi_dense_urban_cluster",
+        "suburban_or_peri_urban",
+        "rural_cluster",
+        "low_density_rural",
+        "very_low_density_rural",
+        "water",
+    ):
+        assert len(indicator.covariates[key]) == 9
+        for i in indicator.covariates[key]:
+            assert i is not None
+            assert i >= 0
+    # Calculate
+    indicator.calculate()
+    assert isinstance(indicator.building_area_prediction, list)
+    assert len(indicator.building_area_prediction) >= 0
+    assert isinstance(indicator.result.timestamp_osm, datetime)
+    assert isinstance(indicator.result.timestamp_oqt, datetime)
+    assert indicator.result.label is not None
+    assert indicator.result.value is not None
+    assert indicator.result.description is not None
+    assert indicator.result.value <= 1.0
+    assert indicator.result.value >= 0.0
+    # Create Figure
+    indicator.create_figure()
+    assert indicator.result.svg is not None
+
+    def test_get_smod_class_share(mock_env_oqt_data_dir, feature):
+        result = get_smod_class_share(FeatureCollection(features=[feature]))
+        assert result == {
+            "urban_centre": [0.05128205128205128],
+            "dense_urban_cluster": [0],
+            "semi_dense_urban_cluster": [0],
+            "suburban_or_peri_urban": [0.029914529914529916],
+            "rural_cluster": [0],
+            "low_density_rural": [0.017094017094017096],
+            "very_low_density_rural": [0.9017094017094017],
+            "water": [0],
+        }
+
+    def test_get_hex_cells(feature):
+        result = asyncio.run(get_hex_cells(feature))
+        assert isinstance(result, FeatureCollection)
+        assert result.features is not None
+
+    def test_get_hex_cells_not_found(feature):
+        feature = get_geojson_fixture("heidelberg-altstadt-feature.geojson")
+        with pytest.raises(HexCellsNotFoundError):
             asyncio.run(get_hex_cells(feature))
 
-    def test_get_shdi(self):
-        result = asyncio.run(get_shdi(FeatureCollection(features=[self.feature])))
-        self.assertIsInstance(result, list)
-        self.assertEqual(len(result), 1)
+    def test_get_shdi(feature):
+        result = asyncio.run(get_shdi(FeatureCollection(features=[feature])))
+        assert isinstance(result, list)
+        assert len(result) == 1
 
 
 if __name__ == "__main__":
