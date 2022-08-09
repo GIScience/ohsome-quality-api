6493b0f3bffc069f82b9d1db39fb344ed3c0199a
Matthias Schaub <matthias.schaub@heigit.org>
2022-08-09 15:51:37 +0200

    fix: pass down include_svg/html to as_geojson func

--- workers/ohsome_quality_analyst/oqt.py
+++ workers/ohsome_quality_analyst/oqt.py
@@ -72,7 +72,13 @@ async def _(
         tasks.append(create_indicator(parameters.copy(update={"bpolys": feature})))
     indicators = await gather_with_semaphore(tasks)
     features = [
-        i.as_feature(parameters.flatten, parameters.include_data) for i in indicators
+        i.as_feature(
+            parameters.flatten,
+            parameters.include_data,
+            parameters.include_svg,
+            parameters.include_html,
+        )
+        for i in indicators
     ]
     if len(features) == 1:
         return features[0]
@@ -204,8 +210,6 @@ async def _(
 @create_indicator.register
 async def _(
     parameters: IndicatorBpolys,
-    include_html: bool = False,
-    include_svg: bool = False,
     *_args,
 ) -> Indicator:
     """Create an indicator from scratch."""
@@ -226,8 +230,8 @@ async def _(
     logging.info("Run calculation")
     indicator.calculate()
     logging.info("Run figure creation")
-    indicator.create_figure(include_svg=include_svg)
-    indicator.create_html(include_html=include_html)
+    indicator.create_figure(include_svg=parameters.include_svg)
+    indicator.create_html(include_html=parameters.include_html)
 
     return indicator
 
