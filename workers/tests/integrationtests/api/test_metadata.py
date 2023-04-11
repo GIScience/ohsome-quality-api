210749690b125fbf92939c69e2a219fa5ad6814d
Johannes Visintini <johannes.visintini@heigit.org>
2023-04-11 17:38:34 +0200

    test(api): add test for /metadata endpoint

---
+++ workers/tests/integrationtests/api/test_metadata.py
@@ -0,0 +1,69 @@
+import pytest
+
+
+def test_metadata(
+    client, response_template, building_count, metadata_mapping_saturation
+):
+    response = client.get("/metadata")
+    assert response.status_code == 200
+
+    content = response.json()
+    result = content.pop("result")
+    assert content == response_template
+    # check topics result
+    assert building_count == next(
+        filter(lambda t: t["key"] == "building_count", result["topics"])
+    )
+    # check indicators result
+    # TODO: change to t["key"]
+    assert metadata_mapping_saturation == next(
+        filter(lambda t: t["name"] == "Mapping Saturation", result["indicators"])
+    )
+    # check reports result
+    # no report in core yet
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_project_core(
+    client, response_template, building_count, metadata_mapping_saturation
+):
+    response = client.get("/metadata?project=core")
+    assert response.status_code == 200
+
+    content = response.json()
+    result = content.pop("result")
+    assert content == response_template
+    # check topics result
+    assert building_count == next(
+        filter(lambda t: t["key"] == "building_count", result["topics"])
+    )
+    # check indicators result
+    # TODO: change to t["key"]
+    assert metadata_mapping_saturation == next(
+        filter(lambda t: t["name"] == "Mapping Saturation", result["indicators"])
+    )
+    # check reports result
+    # no report in core yet
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_project_misc(
+    client, response_template, metadata_topic_minimal, metadata_indicator_minimal
+):
+    response = client.get("/metadata?project=misc")
+    assert response.status_code == 200
+
+    content = response.json()
+    result = content.pop("result")
+    assert content == response_template
+    # check topics result
+    assert metadata_topic_minimal == next(
+        filter(lambda t: t["key"] == "minimal", result["topics"])
+    )
+    # check indicators result
+    # TODO: change to t["key"]
+    assert metadata_indicator_minimal == next(
+        filter(lambda t: t["name"] == "Minimal", result["indicators"])
+    )
+    # check reports result
+    # no report in core yet
