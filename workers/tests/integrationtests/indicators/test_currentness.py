bfc2f77c99912969bbf61ed6f149ee654077adf0
Gigaszi <levi@szamek.de>
2023-04-18 17:08:44 +0200

    test: refactor tests

--- workers/tests/integrationtests/indicators/test_currentness.py
+++ workers/tests/integrationtests/indicators/test_currentness.py
@@ -1,11 +1,12 @@
 import asyncio
 import os
-import unittest
 from datetime import datetime
 
 import geojson
+import plotly.graph_objects as pgo
+import plotly.io as pio
+import pytest
 
-from ohsome_quality_analyst.geodatabase import client as db_client
 from ohsome_quality_analyst.indicators.currentness.indicator import (
     Currentness,
     get_last_edited_year,
@@ -14,73 +15,96 @@ from ohsome_quality_analyst.indicators.currentness.indicator import (
 from tests.integrationtests.utils import get_topic_fixture, oqt_vcr
 
 
-class TestIndicatorCurrentness(unittest.TestCase):
-    @oqt_vcr.use_cassette()
-    def test(self):
-        # Heidelberg
-        feature = asyncio.run(
-            db_client.get_feature_from_db(dataset="regions", feature_id="3")
-        )
-
-        indicator = Currentness(
-            feature=feature,
-            topic=get_topic_fixture("major_roads_count"),
-        )
-        self.assertIsNotNone(indicator.attribution())
-
+class TestPreprocess:
+    @oqt_vcr.use_cassette
+    def test_preprocess(self, topic_building_count, feature_germany_heidelberg):
+        indicator = Currentness(topic_building_count, feature_germany_heidelberg)
         asyncio.run(indicator.preprocess())
-        self.assertIsInstance(indicator.result.timestamp_osm, datetime)
-        self.assertIsInstance(indicator.result.timestamp_oqt, datetime)
-        indicator.calculate()
-        self.assertIsNotNone(indicator.result.label)
-        self.assertIsNotNone(indicator.result.value)
-        self.assertIsNotNone(indicator.result.description)
+        assert len(indicator.contributions_abs) > 0
+        assert isinstance(indicator.result.timestamp_oqt, datetime)
+        assert isinstance(indicator.result.timestamp_osm, datetime)
 
+
+class TestCalculation:
+    @pytest.fixture(scope="class")
+    @oqt_vcr.use_cassette
+    def indicator(self, topic_building_count, feature_germany_heidelberg):
+        i = Currentness(topic_building_count, feature_germany_heidelberg)
+        asyncio.run(i.preprocess())
+        i.calculate()
+        return i
+
+    def test_calculate(self, indicator):
+        assert indicator.result.value >= 0.0
+        assert indicator.result.label in ["green", "yellow", "red", "undefined"]
+        assert indicator.result.description is not None
+
+        assert isinstance(indicator.result.timestamp_osm, datetime)
+        assert isinstance(indicator.result.timestamp_oqt, datetime)
+
+
+class TestFigure:
+    @pytest.fixture(scope="class")
+    @oqt_vcr.use_cassette
+    def indicator(self, topic_building_count, feature_germany_heidelberg):
+        i = Currentness(topic_building_count, feature_germany_heidelberg)
+        asyncio.run(i.preprocess())
+        i.calculate()
+        return i
+
+    @pytest.mark.skip(reason="Only for manual testing.")  # comment for manual test
+    def test_create_figure_manual(self, indicator):
         indicator.create_figure()
-        self.assertIsNotNone(indicator.result.svg)
+        pio.show(indicator.result.figure)
 
-    @oqt_vcr.use_cassette()
-    def test_no_amenities(self):
-        """Test area with no amenities"""
-        infile = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "../fixtures",
-            "niger-kanan-bakache.geojson",
-        )
-        with open(infile, "r") as f:
-            feature = geojson.load(f)
-
-        indicator = Currentness(feature=feature, topic=get_topic_fixture("amenities"))
-        asyncio.run(indicator.preprocess())
-        self.assertEqual(indicator.element_count, 0)
-
-        indicator.calculate()
-        self.assertEqual(indicator.result.label, "undefined")
-        self.assertEqual(indicator.result.value, None)
-
-    def test_get_last_edited_year(self):
-        given = {"2008": 3, "2009": 0, "2010": 5, "2011": 0}
-        expected = 2010
-        result = get_last_edited_year(given)
-        assert result == expected
-
-    def test_get_last_edited_year_unsorted(self):
-        given = {"2008": 3, "2010": 5, "2009": 0, "2011": 0}
-        expected = 2010
-        result = get_last_edited_year(given)
-        assert result == expected
-
-    def test_get_median_year(self):
-        given = {"2008": 0.2, "2009": 0, "2010": 0.6, "2011": 0.2}
-        expected = 2010
-        result = get_median_year(given)
-        assert result == expected
-
-        given = {"2008": 0.6, "2009": 0, "2010": 0.2, "2011": 0.2}
-        expected = 2008
-        result = get_median_year(given)
-        assert result == expected
+    def test_create_figure(self, indicator):
+        indicator.create_figure()
+        assert isinstance(indicator.result.figure, dict)
+        pgo.Figure(indicator.result.figure)  # test for valid Plotly figure
+        assert indicator.result.svg is not None
 
 
-if __name__ == "__main__":
-    unittest.main()
+@oqt_vcr.use_cassette()
+def test_no_amenities():
+    """Test area with no amenities"""
+    infile = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "../fixtures",
+        "niger-kanan-bakache.geojson",
+    )
+    with open(infile, "r") as f:
+        feature = geojson.load(f)
+
+    indicator = Currentness(feature=feature, topic=get_topic_fixture("amenities"))
+    asyncio.run(indicator.preprocess())
+    assert indicator.element_count == 0
+
+    indicator.calculate()
+    assert indicator.result.label == "undefined"
+    assert indicator.result.value is None
+
+
+def test_get_last_edited_year():
+    given = {"2008": 3, "2009": 0, "2010": 5, "2011": 0}
+    expected = 2010
+    result = get_last_edited_year(given)
+    assert result == expected
+
+
+def test_get_last_edited_year_unsorted():
+    given = {"2008": 3, "2010": 5, "2009": 0, "2011": 0}
+    expected = 2010
+    result = get_last_edited_year(given)
+    assert result == expected
+
+
+def test_get_median_year():
+    given = {"2008": 0.2, "2009": 0, "2010": 0.6, "2011": 0.2}
+    expected = 2010
+    result = get_median_year(given)
+    assert result == expected
+
+    given = {"2008": 0.6, "2009": 0, "2010": 0.2, "2011": 0.2}
+    expected = 2008
+    result = get_median_year(given)
+    assert result == expected
