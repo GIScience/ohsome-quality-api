003f8af3883a7220d6302267077231a6173f5aa6
Johannes Visintini <johannes.visintini@heigit.org>
2023-04-25 11:42:00 +0200

    test(models): add tests for quality dimension model

---
+++ workers/tests/unittests/test_quality_dimension.py
@@ -0,0 +1,24 @@
+import pytest
+from pydantic import ValidationError
+
+from ohsome_quality_analyst.quality_dimensions.models import QualityDimension
+
+
+def test_quality_dimension():
+    QualityDimension(name="My fancy QualityDimension", description="some description")
+
+
+def test_parameter_missing():
+    with pytest.raises(ValidationError):
+        QualityDimension(name="some name")
+    with pytest.raises(ValidationError):
+        QualityDimension(description="description")
+
+
+def test_extra_parameter():
+    with pytest.raises(ValidationError):
+        QualityDimension(
+            name="My fancy QualityDimension",
+            description="some description",
+            foo="bar",
+        )
