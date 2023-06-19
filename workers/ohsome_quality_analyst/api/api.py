79d7b7384f28321dadc6282a94b03d78f23934cc
Johannes Visintini <johannes.visintini@heigit.org>
2023-06-19 17:14:06 +0200

    feat(swagger): add OQT favicon to swagger UI

--- workers/ohsome_quality_analyst/api/api.py
+++ workers/ohsome_quality_analyst/api/api.py
@@ -167,6 +167,7 @@ async def custom_swagger_ui_html():
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="/static/swagger-ui-bundle.js",
         swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/favicon-32x32.png",
     )
 
 
@@ -181,6 +182,7 @@ async def redoc_html():
         openapi_url=app.openapi_url,
         title=app.title + " - ReDoc",
         redoc_js_url="/static/redoc.standalone.js",
+        redoc_favicon_url="/static/favicon-32x32.png",
     )
 
 
