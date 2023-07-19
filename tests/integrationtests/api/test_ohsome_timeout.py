72f3c3f79dcd9f14b6ea7fde108ab58c5654419c
Matthias Schaub <matthias.schaub@heigit.org>
2023-07-19 15:09:30 +0200

    test: refactor to pytest and clean-up fixtures

--- tests/integrationtests/api/test_ohsome_timeout.py
+++ tests/integrationtests/api/test_ohsome_timeout.py
@@ -8,59 +8,47 @@ Tests for the individual endpoints and using the `bpolys` parameter please see:
     - `test_api_report_geojson_io.py`
 """
 import os
-import unittest
 from unittest import mock
 
 import httpx
-from fastapi.testclient import TestClient
 
-from ohsome_quality_analyst.api.api import app
-from tests.integrationtests.utils import AsyncMock, get_geojson_fixture
+from tests.integrationtests.utils import AsyncMock
 
 
-class TestApiOhsomeTimeout(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
-
-    def test_ohsome_timeout(self):
-        path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "..",
-            "..",
-            "unittests",
-            "fixtures",
-            "ohsome-response-200-invalid.geojson",
+def test_ohsome_timeout(client, bpolys):
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "..",
+        "..",
+        "unittests",
+        "fixtures",
+        "ohsome-response-200-invalid.geojson",
+    )
+    with open(path, "r") as f:
+        invalid_response = f.read()
+    with mock.patch("httpx.AsyncClient.post", new_callable=AsyncMock) as mock_request:
+        mock_request.return_value = httpx.Response(
+            200,
+            content=invalid_response,
+            request=httpx.Request("POST", "https://www.example.org/"),
         )
-        with open(path, "r") as f:
-            invalid_response = f.read()
-        featurecollection = get_geojson_fixture(
-            "heidelberg-bahnstadt-bergheim-featurecollection.geojson"
-        )
-        with mock.patch(
-            "httpx.AsyncClient.post", new_callable=AsyncMock
-        ) as mock_request:
-            mock_request.return_value = httpx.Response(
-                200,
-                content=invalid_response,
-                request=httpx.Request("POST", "https://www.example.org/"),
-            )
 
-            for endpoint, parameters in (
-                (
-                    "/reports/minimal",
-                    {
-                        "bpolys": featurecollection,
-                    },
-                ),
-                (
-                    "/indicators/minimal",
-                    {
-                        "bpolys": featurecollection,
-                        "topic": "minimal",
-                    },
-                ),
-            ):
-                response = self.client.post(endpoint, json=parameters)
-                self.assertEqual(response.status_code, 422)
-                content = response.json()
-                self.assertEqual(content["type"], "OhsomeApiError")
+        for endpoint, parameters in (
+            (
+                "/reports/minimal",
+                {
+                    "bpolys": bpolys,
+                },
+            ),
+            (
+                "/indicators/minimal",
+                {
+                    "bpolys": bpolys,
+                    "topic": "minimal",
+                },
+            ),
+        ):
+            response = client.post(endpoint, json=parameters)
+            assert response.status_code == 422
+            content = response.json()
+            assert content["type"] == "OhsomeApiError"
