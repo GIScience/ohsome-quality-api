b1e6717d32933d95133f438e3062d72d5cbd6d48
Gigaszi <levi@szamek.de>
2023-07-25 11:35:41 +0200

    WIP

--- ohsome_quality_analyst/utils/validators.py
+++ ohsome_quality_analyst/utils/validators.py
@@ -12,6 +12,12 @@ from ohsome_quality_analyst.utils.exceptions import (
 from ohsome_quality_analyst.utils.helper_geo import calculate_area
 
 
+class InvalidCRSError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+
 def validate_indicator_topic_combination(indicator: str, topic: str):
     if indicator not in get_valid_indicators(topic):
         raise IndicatorTopicCombinationError(indicator, topic)
@@ -29,6 +35,17 @@ def validate_geojson(bpolys: GeoJSON):
             raise GeoJSONGeometryTypeError()
     else:
         raise GeoJSONObjectTypeError()
+    crs = bpolys.get("crs")
+    if crs is None:
+        pass
+    elif "urn:ogc:def:crs:OGC::CRS84" in crs["properties"]["name"]:
+        pass
+    else:
+        error_code = 400
+        error_message = (
+            "Invalid CRS. The FeatureCollection must have the EPSG:4326 CRS or none."
+        )
+        raise InvalidCRSError(error_message, error_code)
 
 
 def validate_area(feature: Feature):
