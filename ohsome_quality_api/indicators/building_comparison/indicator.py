fb6eb3ef1dc79589e679f15b30275f19738c50e9
hn437 <Danner@stud.uni-heidelberg.de>
2024-02-05 08:39:52 +0100

    fixed bug in coverage function by deleting unnecessary functions

--- ohsome_quality_api/indicators/building_comparison/indicator.py
+++ ohsome_quality_api/indicators/building_comparison/indicator.py
@@ -60,9 +60,7 @@ class BuildingComparison(BaseIndicator):
                 table = val["coverage"]["inversed"]
             else:
                 table = val["coverage"]["simple"]
-            feature_str = await db_client.get_reference_coverage(table)
-            geojson_dict = geojson.loads(feature_str)
-            feature = Feature(geometry=geojson_dict, properties={})
+            feature = await db_client.get_reference_coverage(table)
             feature.properties.update({"refernce_dataset": val["name"]})
             features.append(feature)
             return features
