765afb80e107bfc6211438de299ee62b6f82e657
hn437 <Danner@stud.uni-heidelberg.de>
2024-02-26 15:41:10 +0100

    adapted to use inputs instead of hardcoded bpoly and table name

--- ohsome_quality_api/geodatabase/get_matched_roads.sql
+++ ohsome_quality_api/geodatabase/get_matched_roads.sql
@@ -1,40 +1,15 @@
 ---- OQAPI Query
 WITH bpoly AS (
     SELECT
-        ST_Setsrid (ST_GeomFromGeoJSON ('{
-        "coordinates": [
-          [
-            [
-              14.401600308089286,
-              35.98854348048188
-            ],
-            [
-              14.401600308089286,
-              35.9033309465853
-            ],
-            [
-              14.495720596358126,
-              35.9033309465853
-            ],
-            [
-              14.495720596358126,
-              35.98854348048188
-            ],
-            [
-              14.401600308089286,
-              35.98854348048188
-            ]
-          ]
-        ],
-        "type": "Polygon"
-      }'), 4326) AS geom
+        ST_Setsrid (ST_GeomFromGeoJSON (%s), 4326) AS geom
 )
 SELECT
-    SUM(length_osm_buffer_10) / SUM(length)::float8 AS ratio
+    SUM(length_osm_buffer_10),
+    SUM(length)
 FROM
-    oqapi.microsoft_roads_europe_2022_06_08 ms,
+    {table_name},
     bpoly
 WHERE
-    ST_Intersects (ms.geom, bpoly.geom)
+    ST_Intersects ({table_name}.geom, bpoly.geom)
     -- middle point of line within AOI
-    AND ST_Within (ST_LineInterpolatePoint (ms.geom, 0.5), bpoly.geom);
+    AND ST_Within (ST_LineInterpolatePoint ({table_name}.geom, 0.5), bpoly.geom);
