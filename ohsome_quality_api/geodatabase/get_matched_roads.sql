4fa1b529b8a0b5c6dabcf89a4b558458166251c9
Gigaszi <levi@szamek.de>
2024-04-17 17:11:19 +0200

    fix: make query run for new database

--- ohsome_quality_api/geodatabase/get_matched_roads.sql
+++ ohsome_quality_api/geodatabase/get_matched_roads.sql
@@ -1,12 +1,12 @@
 ---- OQAPI Query
+
 WITH bpoly AS (
     SELECT
         ST_Setsrid(ST_GeomFromGeoJSON(%s), 4326) AS geom
 )
 SELECT
-    SUM(mrc.covered),
-    SUM(mrc.length)
+    SUM(cr.covered),
+    SUM(cr.length)
 FROM
     bpoly
-LEFT JOIN {table_name_features} cr ON ST_Intersects(cr.geom, bpoly.geom) AND ST_Within(ST_LineInterpolatePoint(cr.geom, 0.5), bpoly.geom)
-LEFT JOIN {table_name_stats} mrc ON cr.id = mrc.id;
+LEFT JOIN {table_name} cr ON ST_Intersects(cr.midpoint, bpoly.geom);
