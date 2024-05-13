6d1fdd3a9af79dd95c2915e0b41facea2899e9e0
Gigaszi <levi@szamek.de>
2024-05-13 13:24:09 +0200

    feat: add init function to base indicator class

--- ohsome_quality_api/oqt.py
+++ ohsome_quality_api/oqt.py
@@ -77,6 +77,8 @@ async def _create_indicator(
     indicator_class = get_class_from_key(class_type="indicator", key=key)
     indicator = indicator_class(topic, feature)
 
+    logging.info("Run initialization")
+    indicator.init()
     logging.info("Run preprocessing")
     await indicator.preprocess()
     logging.info("Run calculation")
