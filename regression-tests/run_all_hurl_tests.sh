d383da0e9bec62e9c08b99186e2407dfc542fadd
Matthias Merdes <matthias.merdes@heigit.org>
2024-09-10 14:42:59 +0200

    refactor: extract base_url variable from all test to env variable  #820
    https://github.com/GIScience/ohsome-quality-api/issues/820

--- regression-tests/run_all_hurl_tests.sh
+++ regression-tests/run_all_hurl_tests.sh
@@ -1,6 +1,8 @@
 rm -rf report
 mkdir report
 
+export HURL_base_url=https://api.quality.ohsome.org/v1-test
+
 hurl *.hurl   --report-html report
 
 echo "\n\nhurl report: file://$PWD/report/index.html"
