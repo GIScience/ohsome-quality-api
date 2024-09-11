e6e76de63d5a461daa81a9194b174bc00fa11992
Matthias Merdes <mmerdes@users.noreply.github.com>
2024-09-11 15:25:05 +0200

    Incorporate review feedback (batched suggestions) # 820
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>

--- regression-tests/run_hurl_tests_TEST.sh
+++ regression-tests/run_hurl_tests_TEST.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
-export HURL_base_url=https://api.quality.ohsome.org/v1-test
+export HURL_BASE_URL=https://api.quality.ohsome.org/v1-test
 
 cd "$(dirname "$0")"
 ./__run_hurl_tests_for_stage.sh
