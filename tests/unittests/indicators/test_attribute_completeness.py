1f8f3664030052537bfee5035f3645b4fb89a153
JanReifenrath <jtr.linse@gmail.com>
2024-10-31 11:19:27 +0100

    test: finished a unit test for the plot description of the attribute completeness #828

--- tests/unittests/indicators/test_attribute_completeness.py
+++ tests/unittests/indicators/test_attribute_completeness.py
@@ -1,8 +1,25 @@
-from ohsome_quality_api.indicators.attribute_completeness.indicator import AttributeCompleteness
+from ohsome_quality_api.indicators.attribute_completeness.indicator import (
+    AttributeCompleteness,
+)
 
 
 def test_create_description():
-    indicator = AttributeCompleteness() # Grundschritte hier setzen
-    indicator.result = # nötige result Werte hier setzen
-    description = indicator.create_description()
-    assert description == "expected text here"
+    def topic_building_count():
+        pass
+
+    indicator = AttributeCompleteness(
+        topic_building_count,
+        "feature_germany_heidelberg",
+        "attribute_key",
+    )
+    indicator.result.value = 0.2
+    indicator.absolute_value_1 = 10
+    indicator.absolute_value_2 = 2
+    indicator.topic.name = "test-topic"
+    indicator.attribute_key = ["test-attribute"]
+    indicator.create_description()
+    assert indicator.description == (
+        "The ratio of the topic test-topic in "
+        "the selected area (all: 10) compared to the "
+        'topic test-topic with the expected tag "test-attribute" (matched: 2) is 0.2. '
+    )
