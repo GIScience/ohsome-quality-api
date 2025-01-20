387c167e953124fbf66a5227d29e862e49a5a597
Matthias Schaub <matthias.schaub@heigit.org>
2025-01-21 07:36:41 +1300

    feat: add GeoJSON to bounding box helper

--- ohsome_quality_api/utils/helper_geo.py
+++ ohsome_quality_api/utils/helper_geo.py
@@ -1,13 +1,38 @@
+from dataclasses import dataclass
+
 from geojson import Feature
 from geojson.utils import coords
 from pyproj import Geod
 
 
-def calculate_area(feature: Feature) -> float:
-    """Calculate area in meter."""
-    geod = Geod(ellps="WGS84")
+@dataclass
+class BoundingBox:
+    lon_min: float  # left
+    lat_min: float  # bottom
+    lon_max: float  # right
+    lat_max: float  # top
+
+
+def get_coordinates(feature: Feature) -> tuple:
     coordinates = tuple(coords(feature))
     lons = tuple(c[0] for c in coordinates)
     lats = tuple(c[1] for c in coordinates)
+    return lons, lats
+
+
+def calculate_area(feature: Feature) -> float:
+    """Calculate area in meter."""
+    lons, lats = get_coordinates(feature)
+    geod = Geod(ellps="WGS84")
     area, _ = geod.polygon_area_perimeter(lons=lons, lats=lats)
     return area
+
+
+def get_bounding_box(feature: Feature) -> BoundingBox:
+    lons, lats = get_coordinates(feature)
+    return BoundingBox(
+        lon_min=min(lons),
+        lat_min=min(lats),
+        lon_max=max(lons),
+        lat_max=max(lats),
+    )
--- tests/unittests/test_helper_geo.py
+++ tests/unittests/test_helper_geo.py
@@ -1,9 +1,17 @@
+from dataclasses import astuple
+
 import pytest
 
-from ohsome_quality_api.utils.helper_geo import calculate_area
+from ohsome_quality_api.utils.helper_geo import calculate_area, get_bounding_box
 
 
 def test_calculate_area(feature_germany_heidelberg):
     expected = 108852960.62891776  # derived from PostGIS ST_AREA
     result = calculate_area(feature_germany_heidelberg)
     assert result == pytest.approx(expected, abs=1e-3)
+
+
+def test_get_bounding_box(feature_germany_heidelberg):
+    result = get_bounding_box(feature_germany_heidelberg)
+    expected = (8.5731788, 49.3520029, 8.7940496, 49.4596927)
+    assert astuple(result) == pytest.approx(expected, abs=1e-6)
