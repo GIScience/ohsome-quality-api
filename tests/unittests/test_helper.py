c54ac41060620393b73c0eccbea95b92f8140fe8
Matthias Schaub <matthias.schaub@heigit.org>
2025-02-04 14:37:47 +1300

    test: fix testcase

--- tests/unittests/test_helper.py
+++ tests/unittests/test_helper.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import pytest
 
+from ohsome_quality_api.indicators.base import BaseIndicator
 from ohsome_quality_api.indicators.definitions import get_indicator_metadata
 from ohsome_quality_api.indicators.mapping_saturation import models
 from ohsome_quality_api.indicators.minimal.indicator import Minimal
@@ -29,8 +30,9 @@ def test_name_to_class():
 
     indicators = get_indicator_metadata()
     for indicator_name in indicators.keys():
-        assert (
-            get_class_from_key(class_type="indicator", key=indicator_name) is not None
+        assert issubclass(
+            get_class_from_key(class_type="indicator", key=indicator_name),
+            BaseIndicator,
         )
 
 
@@ -107,7 +109,4 @@ def test_hyphen_to_snake():
 
 
 def test_get_module_dir():
-    assert (
-        get_module_dir("ohsome_quality_api.definitions")
-        == "/home/matthias/work/projects/oqapi/ohsome_quality_api"
-    )
+    assert get_module_dir("tests.unittests") == str(Path(__file__).parent)
