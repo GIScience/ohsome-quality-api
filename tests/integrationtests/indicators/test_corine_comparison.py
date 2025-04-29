9323271d411e69e3d30506e7cac3ff60ab193ca6
Gigaszi <levi@szamek.de>
2025-04-29 11:30:52 +0200

    success init of corine-comparison indicator

--- tests/integrationtests/indicators/test_corine_comparison.py
+++ tests/integrationtests/indicators/test_corine_comparison.py
@@ -1,6 +1,8 @@
 from ohsome_quality_api.indicators.corine_comparison.indicator import CorineComparison
+from ohsome_quality_api.topics.models import BaseTopic
 
 
-def test_preprocess():
-    indicator = CorineComparison()
+def test_preprocess(feature_germany_heidelberg):
+    topic = BaseTopic(key="forest", name="forest", description="forest")
+    indicator = CorineComparison(feature=feature_germany_heidelberg, topic=topic)
     assert indicator is not None
