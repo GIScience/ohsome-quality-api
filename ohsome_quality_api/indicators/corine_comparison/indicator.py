c4319a72815094c8105288cd815a4b68fc9a91c5
Gigaszi <levi@szamek.de>
2025-04-29 11:30:52 +0200

    success init of corine-comparison indicator

--- ohsome_quality_api/indicators/corine_comparison/indicator.py
+++ ohsome_quality_api/indicators/corine_comparison/indicator.py
@@ -1,2 +1,14 @@
-class CorineComparison:
+from indicators.base import BaseIndicator
+
+
+class CorineComparison(BaseIndicator):
     pass
+
+    def preprocess(self) -> None:
+        pass
+
+    def calculate(self) -> None:
+        pass
+
+    def create_figure(self) -> None:
+        pass
