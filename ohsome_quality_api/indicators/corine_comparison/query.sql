030a00a0dbada5d2c60f1d1d5d457d5331d06baa
Matthias Schaub <matthias.schaub@heigit.org>
2025-04-29 12:22:32 +0200

    make sql query postgres compatible

--- ohsome_quality_api/indicators/corine_comparison/query.sql
+++ ohsome_quality_api/indicators/corine_comparison/query.sql
@@ -1,31 +1,16 @@
+-- (8.5, 49.5, 9.0, 50.0)
+WITH bpoly AS (
+    SELECT
+        -- split mutlipolygon into list of polygons for more efficient processing
+        (ST_Dump (ST_SetSRID (ST_GeomFromGeoJSON (%s), 4326))).geom AS geom
+)
 SELECT
-    CASE
-        WHEN ST_Within(
-            geometry,
-            ST_MakeEnvelope(
-                CAST({xmin} AS DOUBLE),
-                CAST({ymin} AS DOUBLE),
-                CAST({xmax} AS DOUBLE),
-                CAST({ymax} AS DOUBLE)
-            )
-        ) THEN area
-        ELSE ST_Intersection(
-            geometry,
-            ST_MakeEnvelope(
-                CAST({xmin} AS DOUBLE),
-                CAST({ymin} AS DOUBLE),
-                CAST({xmax} AS DOUBLE),
-                CAST({ymax} AS DOUBLE)
-            )
-        ).ST_FlipCoordinates().ST_Area_Spheroid()
-    END as area
-    FROM corine_osm_intersection
-    WHERE ST_Intersects(
-        geometry,
-        ST_MakeEnvelope(
-            CAST({xmin} AS DOUBLE),
-            CAST({ymin} AS DOUBLE),
-            CAST({xmax} AS DOUBLE),
-            CAST({ymax} AS DOUBLE)
-        )
-    )
+    CASE WHEN ST_Within (geometry, bpoly) THEN
+        area
+    ELSE
+        ST_Area (ST_Intersection (geometry, bpoly))::geography)
+    END AS area
+FROM
+    osm_corine_intersection
+WHERE
+    ST_Intersects (geometry, bpoly);
