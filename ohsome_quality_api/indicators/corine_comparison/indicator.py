2ea19f574b033320690bcda26fe4300aac98d154
Matthias Schaub <matthias.schaub@heigit.org>
2025-04-29 12:22:32 +0200

    make sql query postgres compatible

--- ohsome_quality_api/indicators/corine_comparison/indicator.py
+++ ohsome_quality_api/indicators/corine_comparison/indicator.py
@@ -1,10 +1,16 @@
-from indicators.base import BaseIndicator
+from pathlib import Path
+import json
+
+from ohsome_quality_api.indicators.base import BaseIndicator
 
 
 class CorineComparison(BaseIndicator):
     pass
 
     def preprocess(self) -> None:
+        with open(Path(__file__).parent / "query.sql", "r") as file:
+            query = file.read().format(json.dumps(self.feature["geometry"]))
+        breakpoint()
         self.areas = []
         self.clc_classes_corine = []
         self.clc_classes_osm = []
