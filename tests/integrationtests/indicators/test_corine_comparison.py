86f4a2931e6306319d64bad329b79a09d409d19d
Gigaszi <levi@szamek.de>
2025-04-29 12:00:49 +0200

    first draft of preprocess function

--- tests/integrationtests/indicators/test_corine_comparison.py
+++ tests/integrationtests/indicators/test_corine_comparison.py
@@ -5,4 +5,10 @@ from ohsome_quality_api.topics.models import BaseTopic
 def test_preprocess(feature_germany_heidelberg):
     topic = BaseTopic(key="forest", name="forest", description="forest")
     indicator = CorineComparison(feature=feature_germany_heidelberg, topic=topic)
-    assert indicator is not None
+    indicator.preprocess()
+    assert isinstance(indicator.areas, list)
+    assert isinstance(indicator.clc_classes_corine, list)
+    assert isinstance(indicator.clc_classes_osm, list)
+    assert len(indicator.areas) != 0
+    for area in indicator.areas:
+        assert area is not None
