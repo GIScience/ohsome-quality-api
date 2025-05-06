ccb61ce2d49c3c4574a9c3a76f956d725f1a9869
Gigaszi <levi@szamek.de>
2025-05-06 15:19:02 +0200

    feat: add query for single clc class

---
+++ ohsome_quality_api/indicators/land_cover_thematic_accuracy/single_class_query.sql
@@ -0,0 +1,19 @@
+WITH bpoly AS (
+    SELECT
+        (ST_Dump(ST_SetSRID(ST_GeomFromGeoJSON($1), 4326))).geom AS geometry
+)
+SELECT
+    CLC_class as clc_class_corine,
+    osm_CLC_class as clc_class_osm,
+    CASE
+        WHEN ST_Within(o.geometry, b.geometry) THEN area
+        ELSE ST_Area(ST_Intersection(o.geometry, b.geometry)::geography)
+    END AS area
+FROM
+    osm_corine_intersection o,
+    bpoly b
+WHERE
+    ST_Intersects(o.geometry, b.geometry)
+    AND (
+        o.CLC_class = $2 OR o.osm_CLC_class = $2
+    );
