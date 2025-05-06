8f51fbacbe22404137059e9b6ce61a9d7057580a
Matthias Schaub <matthias.schaub@heigit.org>
2025-05-06 08:56:19 +0200

    test(approvaltests reporter): create png if not exists

--- tests/approvaltests_reporters.py
+++ tests/approvaltests_reporters.py
@@ -16,6 +16,7 @@ from approvaltests.reporters.python_native_reporter import PythonNativeReporter
 from approvaltests.reporters.report_with_diff_command_line import (
     ReportWithDiffCommandLine,
 )
+from PIL import Image
 
 
 class BlockingGenericDiffReporter(GenericDiffReporter):
@@ -172,6 +173,9 @@ class PlotlyDiffReporter(FirstWorkingReporter):
                 raw = json.load(file)
             approved_figure = pgo.Figure(raw)
             approved_figure.write_image(approved_path_image)
+        else:
+            image = Image.new("RGB", (1, 1))
+            image.save(approved_path_image, "PNG")
 
         with open(received_path, "r") as file:
             raw = json.load(file)
