a88966bc43372331f942ae7fbddc2a7c0e80b7f1
Matthias Schaub <matthias.schaub@heigit.org>
2025-05-08 10:17:06 +0200

    refactor: clearer distinction between multi and single class workflow

--- ohsome_quality_api/indicators/land_cover_thematic_accuracy/indicator.py
+++ ohsome_quality_api/indicators/land_cover_thematic_accuracy/indicator.py
@@ -4,10 +4,7 @@ from pathlib import Path
 
 import plotly.graph_objects as pgo
 from geojson import Feature
-from sklearn.metrics import (
-    classification_report,
-    f1_score,
-)
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 from ohsome_quality_api.api.request_models import CorineClass
 from ohsome_quality_api.geodatabase import client
@@ -30,13 +27,13 @@ class LandCoverThematicAccuracy(BaseIndicator):
 
     async def preprocess(self) -> None:
         if self.corine_class:
-            with open(Path(__file__).parent / "single_class_query.sql", "r") as file:
+            with open(Path(__file__).parent / "query-single-class.sql", "r") as file:
                 query = file.read()
             results = await client.fetch(
                 query, str(self.feature["geometry"]), self.corine_class
             )
         else:
-            with open(Path(__file__).parent / "query.sql", "r") as file:
+            with open(Path(__file__).parent / "query-all-classes.sql", "r") as file:
                 query = file.read()
             results = await client.fetch(query, str(self.feature["geometry"]))
         self.clc_classes_corine = [r["clc_class_corine"] for r in results]
@@ -47,23 +44,27 @@ class LandCoverThematicAccuracy(BaseIndicator):
         self.timestamp_corine = datetime.now(timezone.utc)
 
     def calculate(self) -> None:
-        self.labels = set(self.clc_classes_corine)
-
         self.f1_score = f1_score(
             self.clc_classes_corine,
             self.clc_classes_osm,
             average="weighted",
             sample_weight=self.areas,
         )
-        self.result.value = self.f1_score
-
-        self.f1_scores = f1_score(
+        self.confusion_matrix = confusion_matrix(
             self.clc_classes_corine,
             self.clc_classes_osm,
-            average=None,  # for each
             sample_weight=self.areas,
-            labels=list(self.labels),
+            normalize="all",
         )
+        self.result.value = self.f1_score
+
+        # self.f1_scores = f1_score(
+        #     self.clc_classes_corine,
+        #     self.clc_classes_osm,
+        #     average=None,  # for each
+        #     sample_weight=self.areas,
+        #     labels=set(self.clc_classes_corine),
+        # )
         if self.f1_score > 0.8:
             self.result.class_ = 5
         elif self.f1_score > 0.5:
@@ -88,17 +89,25 @@ class LandCoverThematicAccuracy(BaseIndicator):
             logging.info("Result is undefined. Skipping figure creation.")
             return
 
+        if self.corine_class:
+            self._create_figure_for_single_class()
+        else:
+            self._create_figure_for_all_classes()
+
+    def _create_figure_for_single_class(self):
         value_to_label = {
             cls.value: cls.name.replace("_", " ").title() for cls in CorineClass
         }
-        class_labels = [value_to_label.get(v, str(v)) for v in self.labels]
+        class_labels = [
+            value_to_label.get(v, str(v)) for v in set(self.clc_classes_corine)
+        ]
 
         fig = pgo.Figure(
             data=[
                 pgo.Bar(
                     name="OSM building area",
                     x=class_labels,
-                    y=self.f1_scores,
+                    y=self.f1_score,
                     title="F1-Score of each class",
                 )
             ]
@@ -109,3 +118,22 @@ class LandCoverThematicAccuracy(BaseIndicator):
         raw = fig.to_dict()
         raw["layout"].pop("template")  # remove boilerplate
         self.result.figure = raw
+
+    def _create_figure_for_all_classes(self):
+        fig = pgo.Figure(
+            data=pgo.Heatmap(
+                z=self.confusion_matrix,
+                text=self.confusion_matrix,
+                texttemplate="%{text:.2f}",
+                # TODO: How to choose the biggest/best working one without
+                # committing to hardcoded value
+                textfont={"size": 12},
+            )
+        )
+        fig.update_yaxes(title_text="Corine Land Cover Class")
+        fig.update_xaxes(title_text="Corine Land Cover Class")
+        # TODO add legend with corine land cover classes mapped to meaningful titles?
+
+        raw = fig.to_dict()
+        raw["layout"].pop("template")  # remove boilerplate
+        return raw
