b865ba4e20fac99724fa29234ec12c41b4c105e1
Sylvia <sylvia.pscheidl@heigit.org>
2025-05-20 11:49:02 +0200

    test land cover completeness is running

---
+++ ohsome_quality_api/indicators/land_cover_completeness/indicator.py
@@ -0,0 +1,13 @@
+from indicators.base import BaseIndicator
+
+
+class LandCoverCompleteness(BaseIndicator):
+
+    def preprocess(self):
+        pass
+    def calculate(self):
+        pass
+    def create_figure(self) -> None:
+        pass
+
+
