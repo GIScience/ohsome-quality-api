3e7d3cc193b825dc537ebc404cfed6003a6734c2
Sylvia <sylvia.pscheidl@heigit.org>
2025-06-03 16:26:07 +0200

    working calculate function

--- ohsome_quality_api/indicators/land_cover_completeness/indicator.py
+++ ohsome_quality_api/indicators/land_cover_completeness/indicator.py
@@ -14,17 +14,28 @@ class LandCoverCompleteness(BaseIndicator):
     ) -> None:
         super().__init__(topic=topic, feature=feature)
 
+        self.th_high = 0.85  # Above or equal to this value label should be green
+        self.th_low = 0.50  # Above or equal to this value label should be yellow
+
     async def preprocess(self):
         # get osm building area
 
         result = await ohsome_client.query(self.topic, self.feature)
-        self.osm_area = result["result"][0]["value"] or 0.0  # if None
+        self.osm_area_ratio = result["result"][0]["value"] or 0.0  # if None
         timestamp = result["result"][0]["timestamp"]
         self.result.timestamp_osm = parser.isoparse(timestamp)
 
     def calculate(self):
+        self.osm_area_ratio /= 1000000
+        self.result.value = round(self.osm_area_ratio, 2)
+        if self.result.value >= self.th_high:
+            self.result.class_ = 5
+        elif self.th_high > self.result.value >= self.th_low:
+            self.result.class_ = 3
+        elif self.th_low > self.result.value >= 0:
+            self.result.class_ = 1
         self.result.description = (
-            self.templates.label_description["green"]
+            self.templates.label_description[self.result.label]
             + self.templates.result_description
         )
 
--- tests/integrationtests/indicators/test_land_cover_completeness.py
+++ tests/integrationtests/indicators/test_land_cover_completeness.py
@@ -8,13 +8,15 @@ from ohsome_quality_api.indicators.land_cover_completeness.indicator import (
 
 
 @pytest.mark.asyncio
-async def test_create_land_cover_completeness(topic_land_cover, feature_land_cover):
+async def test_create_land_cover_completeness_preprocess(
+    topic_land_cover, feature_land_cover
+):
     indicator = LandCoverCompleteness(
         topic=topic_land_cover, feature=feature_land_cover
     )
     await indicator.preprocess()
 
-    assert indicator.osm_area is not None
+    assert indicator.osm_area_ratio is not None
     assert indicator.result.timestamp_osm is not None
     verify(indicator.result.description, namer=PytestNamer())
 
@@ -28,4 +30,8 @@ async def test_create_land_cover_completeness_calculate(
     )
     await indicator.preprocess()
     indicator.calculate()
+    assert indicator.osm_area_ratio <= 1
+    assert indicator.result.value is not None
+    assert indicator.result.class_ is not None
+    assert indicator.result.label == "green"
     verify(indicator.result.description, namer=PytestNamer())
