7f792d80b8cb7eebf0200b203d816de8549cac89
Sylvia <sylvia.pscheidl@heigit.org>
2025-06-03 15:15:32 +0200

    add result description

--- ohsome_quality_api/indicators/land_cover_completeness/indicator.py
+++ ohsome_quality_api/indicators/land_cover_completeness/indicator.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
-from geodatabase import client
-from indicators.base import BaseIndicator
-from ohsome_quality_api.ohsome import client as ohsome_client
 from dateutil import parser
-from ohsome_quality_api.topics.models import BaseTopic as Topic
 from geojson import Feature
+from indicators.base import BaseIndicator
+
+from ohsome_quality_api.ohsome import client as ohsome_client
+from ohsome_quality_api.topics.models import BaseTopic as Topic
+
 
 class LandCoverCompleteness(BaseIndicator):
     def __init__(
@@ -22,9 +21,12 @@ class LandCoverCompleteness(BaseIndicator):
         self.osm_area = result["result"][0]["value"] or 0.0  # if None
         timestamp = result["result"][0]["timestamp"]
         self.result.timestamp_osm = parser.isoparse(timestamp)
+
     def calculate(self):
-        pass
+        self.result.description = (
+            self.templates.label_description["green"]
+            + self.templates.result_description
+        )
+
     def create_figure(self) -> None:
         pass
-
-
--- tests/integrationtests/indicators/test_land_cover_completeness.py
+++ tests/integrationtests/indicators/test_land_cover_completeness.py
@@ -1,12 +1,31 @@
 import pytest
+from approvaltests import verify
+from approvaltests_namers import PytestNamer
 
-from conftest import feature_land_cover
-from ohsome_quality_api.indicators.land_cover_completeness.indicator import LandCoverCompleteness
+from ohsome_quality_api.indicators.land_cover_completeness.indicator import (
+    LandCoverCompleteness,
+)
+
 
 @pytest.mark.asyncio
 async def test_create_land_cover_completeness(topic_land_cover, feature_land_cover):
-    indicator = LandCoverCompleteness(topic=topic_land_cover, feature=feature_land_cover)
+    indicator = LandCoverCompleteness(
+        topic=topic_land_cover, feature=feature_land_cover
+    )
     await indicator.preprocess()
 
     assert indicator.osm_area is not None
     assert indicator.result.timestamp_osm is not None
+    verify(indicator.result.description, namer=PytestNamer())
+
+
+@pytest.mark.asyncio
+async def test_create_land_cover_completeness_calculate(
+    topic_land_cover, feature_land_cover
+):
+    indicator = LandCoverCompleteness(
+        topic=topic_land_cover, feature=feature_land_cover
+    )
+    await indicator.preprocess()
+    indicator.calculate()
+    verify(indicator.result.description, namer=PytestNamer())
