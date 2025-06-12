0c9b1b60d77558f52bf17ae54e17b4e3b7089f85
SylviaMaria123 <sylvia.pscheidl@heigit.org>
2025-06-12 11:41:04 +0200

    feat: add land-cover-completeness indicator

---
+++ ohsome_quality_api/indicators/land_cover_completeness/indicator.py
@@ -0,0 +1,48 @@
+import plotly.graph_objects as pgo
+from dateutil import parser
+from geojson import Feature
+
+from ohsome_quality_api.indicators.base import BaseIndicator
+from ohsome_quality_api.ohsome import client as ohsome_client
+from ohsome_quality_api.topics.models import BaseTopic as Topic
+
+
+class LandCoverCompleteness(BaseIndicator):
+    def __init__(
+        self,
+        topic: Topic,
+        feature: Feature,
+    ) -> None:
+        super().__init__(topic=topic, feature=feature)
+
+        self.th_high = 0.85  # Above or equal to this value label should be green
+        self.th_low = 0.50  # Above or equal to this value label should be yellow
+
+    async def preprocess(self):
+        # get osm building area
+
+        result = await ohsome_client.query(self.topic, self.feature)
+        self.osm_area_ratio = result["result"][0]["value"] or 0.0  # if None
+        timestamp = result["result"][0]["timestamp"]
+        self.result.timestamp_osm = parser.isoparse(timestamp)
+
+    def calculate(self):
+        self.osm_area_ratio /= 1000000
+        self.result.value = round(self.osm_area_ratio, 2)
+        if self.result.value >= self.th_high:
+            self.result.class_ = 5
+        elif self.th_high > self.result.value >= self.th_low:
+            self.result.class_ = 3
+        elif self.th_low > self.result.value >= 0:
+            self.result.class_ = 1
+        self.result.description = (
+            self.templates.label_description[self.result.label]
+            + self.templates.result_description
+        )
+
+    def create_figure(self) -> None:
+        fig = pgo.Figure()
+        fig.add_trace(pgo.Bar(x=["name"], y=[self.osm_area_ratio * 100]))
+        raw = fig.to_dict()
+        raw["layout"].pop("template")  # remove boilerplate
+        self.result.figure = raw
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from pathlib import Path
@@ -266,3 +267,33 @@ def metadata_indicator_minimal() -> dict[str, IndicatorMetadata]:
 @pytest.fixture
 def indicators_metadata() -> dict[str, IndicatorMetadata]:
     return get_indicator_metadata()
+
+
+@pytest.fixture
+def feature_land_cover():
+    gj_dict = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [8.63552791262136, 49.711771844660191],
+                    [8.57181432038835, 49.710072815533977],
+                    [8.545479368932039, 49.624271844660186],
+                    [8.685649271844662, 49.642111650485433],
+                    [8.685649271844662, 49.642111650485433],
+                    [8.685649271844662, 49.642111650485433],
+                    [8.63552791262136, 49.711771844660191],
+                ]
+            ],
+        },
+    }
+
+    gj = json.dumps(gj_dict)
+
+    return geojson.loads(gj)
+
+
+@pytest.fixture
+def topic_land_cover() -> TopicDefinition:
+    return get_topic_preset("land-cover")
