04943928b43910271794df6641ca7c2fa145aac7
Matthias Schaub <matthias.schaub@heigit.org>
2025-06-12 11:08:52 +0200

    test: change assert statements to verify calls

--- tests/integrationtests/indicators/test_land_cover_completeness.py
+++ tests/integrationtests/indicators/test_land_cover_completeness.py
@@ -7,47 +7,51 @@ from ohsome_quality_api.indicators.land_cover_completeness.indicator import (
 )
 from tests.approvaltests_namers import PytestNamer
 from tests.approvaltests_reporters import PlotlyDiffReporter
+from tests.integrationtests.utils import oqapi_vcr
 
 
 @pytest.mark.asyncio
+@oqapi_vcr.use_cassette
 async def test_create_land_cover_completeness_preprocess(
     topic_land_cover, feature_land_cover
 ):
     indicator = LandCoverCompleteness(
-        topic=topic_land_cover, feature=feature_land_cover
+        topic=topic_land_cover,
+        feature=feature_land_cover,
     )
     await indicator.preprocess()
-
-    assert indicator.osm_area_ratio is not None
-    assert indicator.result.timestamp_osm is not None
-    verify(indicator.result.description, namer=PytestNamer())
+    verify(indicator.osm_area_ratio, namer=PytestNamer(postfix="osm_area_ratio"))
+    verify(indicator.result.timestamp_osm, namer=PytestNamer(postfix="timestamp_osm"))
 
 
 @pytest.mark.asyncio
+@oqapi_vcr.use_cassette
 async def test_create_land_cover_completeness_calculate(
     topic_land_cover, feature_land_cover
 ):
     indicator = LandCoverCompleteness(
-        topic=topic_land_cover, feature=feature_land_cover
+        topic=topic_land_cover,
+        feature=feature_land_cover,
     )
     await indicator.preprocess()
     indicator.calculate()
-    assert indicator.osm_area_ratio <= 1
-    assert indicator.result.value is not None
-    assert indicator.result.class_ is not None
     assert indicator.result.label == "green"
-    verify(indicator.result.description, namer=PytestNamer())
+    verify(indicator.osm_area_ratio, namer=PytestNamer(postfix="osm_area_ratio"))
+    verify(indicator.result.value, namer=PytestNamer(postfix="value"))
+    verify(indicator.result.class_, namer=PytestNamer(postfix="class_"))
+    verify(indicator.result.description, namer=PytestNamer(postfix="description"))
 
 
 @pytest.mark.asyncio
+@oqapi_vcr.use_cassette
 async def test_create_figure(topic_land_cover, feature_land_cover):
     indicator = LandCoverCompleteness(
-        topic=topic_land_cover, feature=feature_land_cover
+        topic=topic_land_cover,
+        feature=feature_land_cover,
     )
     await indicator.preprocess()
     indicator.calculate()
     indicator.create_figure()
-    assert isinstance(indicator.result.figure, dict)
     verify_as_json(
         to_jsonable_python(indicator.result.figure),
         options=Options().with_reporter(PlotlyDiffReporter()).with_namer(PytestNamer()),
