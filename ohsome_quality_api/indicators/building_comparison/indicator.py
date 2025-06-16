9fa1a772314c5fe19b1a6f29f12adcf832fa27e3
Sylvia <sylvia.pscheidl@heigit.org>
2025-06-16 11:21:03 +0200

    add logo on building comparison figure

--- ohsome_quality_api/indicators/building_comparison/indicator.py
+++ ohsome_quality_api/indicators/building_comparison/indicator.py
@@ -233,9 +233,25 @@ class BuildingComparison(BaseIndicator):
         }
         fig.update_layout(**layout)
 
+        fig.add_layout_image(
+            dict(
+                source="https://media.licdn.com/dms/image/v2/D560BAQE9rkvB7vB_cg/company-logo_200_200/company-logo_200_200/0/1711546373172/heigit_logo?e=2147483647&v=beta&t=pWdgVEOkz7VBhH2WbM5_DJeTs7RsdVXbolKU3ftS1iY",
+                xref="paper",
+                yref="paper",
+                x=0.8,
+                y=-0.052,
+                sizex=0.25,
+                sizey=0.3,
+                sizing="contain",
+                opacity=0.3,
+                layer="above",
+            )
+        )
+
         raw = fig.to_dict()
         raw["layout"].pop("template")  # remove boilerplate
         self.result.figure = raw
+        fig.show()
 
     def check_major_edge_cases(self, dataset: str) -> str:
         """If edge case is present return description if not return empty string."""
