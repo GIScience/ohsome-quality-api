610ae71a01d59f50e43ca2a3012f5fb2fb1bdf14
Matthias Schaub <matthias.schaub@heigit.org>
2025-08-18 16:03:44 +0200

    feat(currentness): optionally query ohsomedb in preprocess function
    instead of ohsome API.
    
    This is implemented as feature flag.

---
+++ ohsome_quality_api/indicators/currentness/query.sql
@@ -0,0 +1,18 @@
+WITH bpoly AS (
+    SELECT
+        ST_Setsrid (ST_GeomFromGeoJSON ('$aoi'), 4326) AS geom
+)
+SELECT
+    Date_trunc('month', valid_from) AS month,
+    Count(*)
+FROM
+    contributions c,
+    bpoly b
+WHERE
+    ST_Intersects (c.geom, b.geom)
+    AND (status_geom_type).status = 'latest'  -- excludes deleted
+    AND ($filter)
+GROUP BY
+    month
+ORDER BY
+    month;
