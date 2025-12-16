9922ceec1ba4dd3bd4bdc6ce132bef42c6206e41
Matthias Schaub <matthias.schaub@heigit.org>
2025-12-16 15:59:10 +0100

    refactor: rename var to avoid shadowing builtin

--- regression-tests/currentness/generate_hurl_files.py
+++ regression-tests/currentness/generate_hurl_files.py
@@ -2,13 +2,13 @@ import json
 from pathlib import Path
 from string import Template
 
-dir = Path(__file__).parent
-with open(dir / "template.hurl", "r") as file:
+dir_ = Path(__file__).parent
+with open(dir_ / "template.hurl", "r") as file:
     hurl_file_template = Template(file.read())
 for topic in ("building-count", "roads"):
     for region in ("heidelberg", "frankfurt", "rhineland-palatinate"):
         for ohsomedb in ("true", "false"):
-            with open(Path(dir / region).with_suffix(".geojson"), "r") as f:
+            with open(Path(dir_ / region).with_suffix(".geojson"), "r") as f:
                 bpolys = json.load(f)
             hurl_file = hurl_file_template.substitute(
                 base_url="https://api.quality.ohsome.org/v1-test",
@@ -21,6 +21,6 @@ for topic in ("building-count", "roads"):
                 ohsomedb_text = "-ohsomedb"
             else:
                 ohsomedb_text = ""
-            path = Path(dir) / f"indicator-{topic}-{region}{ohsomedb_text}.hurl"
+            path = Path(dir_) / f"indicator-{topic}-{region}{ohsomedb_text}.hurl"
             with open(path, "w") as f:
                 f.write(hurl_file)
