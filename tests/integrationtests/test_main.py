da2c8cd2b4edba2c8f643a915ef72b3a7f2cf140
Matthias Schaub <matthias.schaub@heigit.org>
2026-01-14 13:01:32 +0100

    tests: refactor tests into parametrized tests

--- tests/integrationtests/test_main.py
+++ tests/integrationtests/test_main.py
@@ -20,6 +20,11 @@ from tests.integrationtests.utils import oqapi_vcr
             "topic_building_count",
             {"attribute_keys": ["height"]},
         ),
+        (
+            "attribute-completeness",
+            "topic_building_count",
+            {"attribute_keys": ["height", "house-number"]},
+        ),
     ],
 )
 def test_create_indicator_public_feature_collection_single(
@@ -63,17 +68,27 @@ def test_create_indicator_public_feature_collection_multi(
 
 @oqapi_vcr.use_cassette
 @pytest.mark.parametrize(
-    "indicator,topic",
+    "indicator,topic,kwargs",
     [
-        ("minimal", "topic_minimal"),
-        ("mapping-saturation", "topic_building_count"),
-        ("currentness", "topic_building_count"),
+        ("minimal", "topic_minimal", {}),
+        ("mapping-saturation", "topic_building_count", {}),
+        ("currentness", "topic_building_count", {}),
+        (
+            "attribute-completeness",
+            "topic_building_count",
+            {"attribute_keys": ["height"]},
+        ),
+        (
+            "attribute-completeness",
+            "topic_building_count",
+            {"attribute_keys": ["height", "house-number"]},
+        ),
     ],
 )
-def test_create_indicator_private_feature(feature, indicator, topic, request):
+def test_create_indicator_private_feature(feature, indicator, topic, kwargs, request):
     """Test private method to create a single indicator for a single feature."""
     topic = request.getfixturevalue(topic)
-    indicator = asyncio.run(main._create_indicator(indicator, feature, topic))
+    indicator = asyncio.run(main._create_indicator(indicator, feature, topic, **kwargs))
     assert indicator.result.label is not None
     assert indicator.result.value is not None
     assert indicator.result.description is not None
@@ -127,64 +142,3 @@ def test_create_indicator_size_limit_bpolys_data(bpolys):
         },
     )
     asyncio.run(main.create_indicator("mapping-saturation", bpolys, topic))
-
-
-@oqapi_vcr.use_cassette
-def test_create_indicator_public_feature_collection_single_attribute_completeness(
-    bpolys, topic_building_count, attribute_key
-):
-    indicators = asyncio.run(
-        main.create_indicator(
-            "attribute-completeness",
-            bpolys,
-            topic_building_count,
-            attribute_keys=attribute_key,
-        )
-    )
-    assert len(indicators) == 1
-    for indicator in indicators:
-        assert indicator.result.label is not None
-        assert indicator.result.value is not None
-        assert indicator.result.description is not None
-        assert indicator.result.figure is not None
-
-
-@oqapi_vcr.use_cassette
-def test_create_indicator_public_feature_collection_multi_attribute_completeness(
-    feature_collection_heidelberg_bahnstadt_bergheim_weststadt,
-    topic_building_count,
-    attribute_key,
-):
-    """Test create indicators for a feature collection with multiple features."""
-    indicators = asyncio.run(
-        main.create_indicator(
-            "attribute-completeness",
-            feature_collection_heidelberg_bahnstadt_bergheim_weststadt,
-            topic_building_count,
-            attribute_keys=attribute_key,
-        )
-    )
-    assert len(indicators) == 3
-    for indicator in indicators:
-        assert indicator.result.label is not None
-        assert indicator.result.value is not None
-        assert indicator.result.description is not None
-        assert indicator.result.figure is not None
-
-
-@oqapi_vcr.use_cassette
-def test_create_indicator_private_feature_attribute_completeness(
-    feature, topic_building_count, attribute_key
-):
-    indicator = asyncio.run(
-        main._create_indicator(
-            "attribute-completeness",
-            feature,
-            topic_building_count,
-            attribute_keys=attribute_key,
-        )
-    )
-    assert indicator.result.label is not None
-    assert indicator.result.value is not None
-    assert indicator.result.description is not None
-    assert indicator.result.figure is not None
