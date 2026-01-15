d38ffc779c1662d4f6e921b3a16e3d8c1c314489
Levi Szamek <levi.szamek@heigit.org>
2026-01-15 13:34:08 +0100

    fix: correct query for all attributes

--- ohsome_quality_api/indicators/roads_thematic_accuracy/all_attributes.sql
+++ ohsome_quality_api/indicators/roads_thematic_accuracy/all_attributes.sql
@@ -9,7 +9,7 @@ select
         SUM(
             CASE
                 WHEN lanes is not NULL AND fsz is not NULL
-                     AND (name is not NULL or ref is not null) AND nam is not NULL
+                     AND (name is not NULL OR ref is not null) AND nam is not NULL
                      AND oneway is not NULL AND far is not NULL
                      AND surface is not NULL AND ofm is not NULL
                      AND width is not NULL AND brf is not NULL THEN 1
@@ -18,21 +18,37 @@ select
         ) AS present_in_both,
         SUM(
         CASE
-            WHEN lanes is not NULL AND fsz is NULL
-                 AND (name is not NULL or ref is not NULL) AND nam is NULL
-                 AND oneway is not NULL AND far is NULL
-                 AND surface is not NULL AND ofm is NULL
-                 AND width is not NULL AND brf is NULL THEN 1
+            WHEN lanes is not NULL
+                 AND (name is not NULL OR ref is not NULL)
+                 AND oneway is not NULL
+                 AND surface is not NULL
+                 AND width is not NULL
+                 AND (
+                     fsz is NULL
+                     OR nam is NULL
+                     OR far is NULL
+                     OR ofm is NULL
+                     OR brf is NULL
+                     )
+                 THEN 1
             ELSE 0
         END
     ) AS only_osm,
 		SUM(
 			CASE
-				WHEN lanes IS NULL AND fsz IS NOT NULL
-				     AND name is NULL AND ref is NULL AND nam is not NULL
-                     AND oneway is NULL AND far is not NULL
-                     AND surface is NULL AND ofm is not NULL
-                     AND width is NULL AND brf is not NULL THEN 1
+				WHEN fsz IS NOT NULL
+				     AND nam is not NULL
+                     AND far is not NULL
+                     AND ofm is not NULL
+                     AND brf is not NULL
+				     AND (
+				         lanes IS NULL
+				         OR (name is NULL AND ref is NULL)
+				         OR oneway is NULL
+				         OR surface is NULL
+				         OR width is NULL
+				         )
+				    THEN 1
 				ELSE 0
 			end
 		) AS only_dlm,
