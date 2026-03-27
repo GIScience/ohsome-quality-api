c064b63dd6a96a0794ecbffa6039405c0d906406
Matthias Schaub <matthias.schaub@heigit.org>
2026-03-27 12:18:13 +0100

    fix(ohsomedb): create series based on contributions_state.last_timestamp
    instead of NOW()
    
    Use left join with on series to fill data gaps.
    
    Use 0 if SUM aggregation returns None.

--- ohsome_quality_api/ohsomedb/templates/elements.sql
+++ ohsome_quality_api/ohsomedb/templates/elements.sql
@@ -7,53 +7,63 @@ series AS (
     SELECT
         Generate_series(
             '2007-10-01'::timestamp,
-            NOW()::timestamp,
+            contributions_state.last_timestamp::timestamp,
             '1 month'::interval
         ) AS ts
-)
-SELECT
-    {% if aggregation == 'length' %}
-        SUM(
-            CASE
-                WHEN ST_Within(
-                    c.geom,
-                    b.geom
-                )
-                THEN c.length -- Use precomputed area from ohsome-planet
-                ELSE ST_Length(
-                    ST_Intersection(
+    FROM contributions_state
+),
+stats AS (
+    SELECT
+        {% if aggregation == 'length' %}
+            SUM(
+                CASE
+                    WHEN ST_Within(
                         c.geom,
                         b.geom
-                    )::geography
-                )
-            END
-        )::BIGINT
-    {% elif aggregation == 'area' or aggregation == 'area\density' %}
-        SUM(
-            CASE
-                WHEN ST_Within(
-                    c.geom,
-                    b.geom,
-                )
-                THEN c.area -- Use precomputed area from ohsome-planet
-                ELSE ST_Area(
-                    ST_Intersection(
+                    )
+                    THEN c.length -- Use precomputed area from ohsome-planet
+                    ELSE ST_Length(
+                        ST_Intersection(
+                            c.geom,
+                            b.geom
+                        )::geography
+                    )
+                END
+            )::BIGINT
+        {% elif aggregation == 'area' or aggregation == 'area\density' %}
+            SUM(
+                CASE
+                    WHEN ST_Within(
                         c.geom,
                         b.geom,
-                    )::geography
-                )
-            END
-        )::BIGINT
-    {% else %}
-        COUNT(*)
-    {% endif %}
-        AS element,
-    ts AS timestamp
-FROM {{ contributions }} c, series s, bpoly b
-WHERE 1=1
-    -- ohsome-filter-to-sql generated clause
-    AND ({{ filter }})
-    AND ST_Intersects(c.geom, b.geom)
-    AND c.valid_from <= s.ts AND s.ts < c.valid_to
-GROUP BY ts
-ORDER BY ts;
+                    )
+                    THEN c.area -- Use precomputed area from ohsome-planet
+                    ELSE ST_Area(
+                        ST_Intersection(
+                            c.geom,
+                            b.geom,
+                        )::geography
+                    )
+                END
+            )::BIGINT
+        {% else %}
+            COUNT(*)
+        {% endif %}
+            AS element,
+        s.ts
+    FROM {{ contributions }} c, series s, bpoly b
+    WHERE 1=1
+        -- ohsome-filter-to-sql generated clause
+        AND ({{ filter }})
+        -- include only valid stat (exclude states such as deleted and invalid)
+        AND (status_geom_type).status in ('history', 'latest')
+        AND ST_Intersects(c.geom, b.geom)
+        AND c.valid_from <= s.ts AND s.ts < c.valid_to
+    GROUP BY ts
+   )
+SELECT
+    series.ts AS timestamp,
+    COALESCE(stats.element, 0) AS element
+FROM
+    series LEFT JOIN stats ON series.ts = stats.ts
+ORDER BY timestamp;
