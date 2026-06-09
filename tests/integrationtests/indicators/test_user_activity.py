3c622ad42420a7d8281829408191b829aa682060
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-09 07:29:31 +0200

    tests: add vcr cassettes

--- tests/integrationtests/indicators/test_user_activity.py
+++ tests/integrationtests/indicators/test_user_activity.py
@@ -4,10 +4,12 @@ import pytest
 from pytest_approval.main import verify_plotly
 
 from ohsome_quality_api.indicators.user_activity.indicator import UserActivity
+from tests.integrationtests.utils import oqapi_vcr
 
 pytestmark = pytest.mark.asyncio
 
 
+@oqapi_vcr.use_cassette
 async def test_preprocess(
     topic_building_count,
     feature_germany_heidelberg,
@@ -19,6 +21,7 @@ async def test_preprocess(
     assert isinstance(indicator.result.timestamp_osm, datetime)
 
 
+@oqapi_vcr.use_cassette
 async def test_create_figure(topic_building_count, feature_germany_heidelberg):
     indicator = UserActivity(topic_building_count, feature_germany_heidelberg)
     await indicator.preprocess()
