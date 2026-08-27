5c014bdafe174266444c190ff1dfd9533a897270
Matthias Schaub <matthias.schaub@heigit.org>
2026-08-27 13:18:27 +0200

    scripts: remove unused update swagger script

--- scripts/update_swagger_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"/../ohsome_quality_api/api/static && \
-  wget https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js -O swagger-ui-bundle.js && \
-  wget https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css -O swagger-ui.css && \
-  wget https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js -O redoc.standalone.js
